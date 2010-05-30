39d63822f3b66986c7000c06a974cf8af70fe606
Matt Westcott <matt@west.co.tt>
2010-05-30 21:40:25 +0000

    Add platform field to production form

--- demoscene/forms.py
+++ demoscene/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from demoscene.models import Production, ProductionType
+from demoscene.models import Production, ProductionType, Platform
 
 class ProductionForm(forms.ModelForm):
 	class Meta:
@@ -7,6 +7,11 @@ class ProductionForm(forms.ModelForm):
 		fields = ('title', )
 		
 class ProductionTypeForm(forms.Form):
-	production_type = forms.ModelChoiceField(queryset = ProductionType.objects.all())
+	production_type = forms.ModelChoiceField(queryset = ProductionType.objects.order_by('name'))
 
 ProductionTypeFormSet = forms.formsets.formset_factory(ProductionTypeForm, can_delete = True)
+
+class ProductionPlatformForm(forms.Form):
+	platform = forms.ModelChoiceField(queryset = Platform.objects.order_by('name'))
+
+ProductionPlatformFormSet = forms.formsets.formset_factory(ProductionPlatformForm, can_delete = True)
--- demoscene/views/productions.py
+++ demoscene/views/productions.py
@@ -1,6 +1,6 @@
 from demoscene.shortcuts import *
 from demoscene.models import Production
-from demoscene.forms import ProductionForm, ProductionTypeFormSet
+from demoscene.forms import ProductionForm, ProductionTypeFormSet, ProductionPlatformFormSet
 
 def index(request):
 	productions = Production.objects.order_by('title')
@@ -19,35 +19,44 @@ def edit(request, production_id):
 	if request.method == 'POST':
 		form = ProductionForm(request.POST, instance = production)
 		production_type_formset = ProductionTypeFormSet(request.POST, prefix = 'prod_type')
-		if form.is_valid() and production_type_formset.is_valid():
+		production_platform_formset = ProductionPlatformFormSet(request.POST, prefix = 'prod_platform')
+		if form.is_valid() and production_type_formset.is_valid() and production_platform_formset.is_valid():
 			form.save()
 			production.types = get_production_types(production_type_formset)
+			production.platforms = get_production_platforms(production_platform_formset)
 			return redirect('production', args = [production.id])
 	else:
 		form = ProductionForm(instance = production)
 		production_type_formset = ProductionTypeFormSet(prefix = 'prod_type',
 			initial = [{'production_type': typ.id} for typ in production.types.all()])
+		production_platform_formset = ProductionPlatformFormSet(prefix = 'prod_platform',
+			initial = [{'platform': platform.id} for platform in production.platforms.all()])
 	
 	return render(request, 'productions/edit.html', {
 		'production': production,
 		'form': form,
 		'production_type_formset': production_type_formset,
+		'production_platform_formset': production_platform_formset,
 	})
 
 def create(request):
 	if request.method == 'POST':
 		form = ProductionForm(request.POST)
 		production_type_formset = ProductionTypeFormSet(request.POST, prefix = 'prod_type')
-		if form.is_valid() and production_type_formset.is_valid():
+		production_platform_formset = ProductionPlatformFormSet(request.POST, prefix = 'prod_platform')
+		if form.is_valid() and production_type_formset.is_valid() and production_platform_formset.is_valid():
 			production = form.save()
 			production.types = get_production_types(production_type_formset)
+			production.platforms = get_production_platforms(production_platform_formset)
 			return redirect('production', args = [production.id])
 	else:
 		form = ProductionForm()
 		production_type_formset = ProductionTypeFormSet(prefix = 'prod_type')
+		production_platform_formset = ProductionPlatformFormSet(prefix = 'prod_platform')
 	return render(request, 'productions/create.html', {
 		'form': form,
 		'production_type_formset': production_type_formset,
+		'production_platform_formset': production_platform_formset,
 	})
 
 # helper functions
@@ -60,3 +69,13 @@ def get_production_types(production_type_formset):
 		if prod_type_form.cleaned_data.get('production_type') and prod_type_form.cleaned_data['production_type'] in prod_types:
 			prod_types.remove(prod_type_form.cleaned_data['production_type'])
 	return prod_types
+
+def get_production_platforms(production_platform_formset):
+	platforms = []
+	for prod_platform_form in production_platform_formset.forms:
+		if prod_platform_form.cleaned_data.get('platform'):
+			platforms.append(prod_platform_form.cleaned_data['platform'])
+	for prod_platform_form in production_platform_formset.deleted_forms:
+		if prod_platform_form.cleaned_data.get('platform') and prod_platform_form.cleaned_data['platform'] in platforms:
+			platforms.remove(prod_platform_form.cleaned_data['platform'])
+	return platforms
