7d63a548988ec8bafe87a96bf5cd99f851bdab47
Matt Westcott <matt@west.co.tt>
2010-07-30 23:22:42 +0000

    refactor scener-with-affiliations snippet to be used in sceners listing. Also MOAR icons.

--- demoscene/templatetags/demoscene_tags.py
+++ demoscene/templatetags/demoscene_tags.py
@@ -1,8 +1,25 @@
 from django import template
 from django.core.urlresolvers import reverse
 
+from demoscene.models import Releaser, Nick
+
 register = template.Library()
 
 @register.inclusion_tag('shared/nick_variants.html')
 def nick_variants(nick):
 	return {'variants': nick.nick_variant_list}
+
+@register.inclusion_tag('shared/scener_with_affiliations.html')
+def scener_with_affiliations(scener):
+	if isinstance(scener, Nick):
+		return {
+			'name': scener.name,
+			'releaser': scener.releaser,
+			'groups': scener.releaser.groups.all()
+		}
+	else: # assume a Releaser
+		return {
+			'name': scener.name,
+			'releaser': scener,
+			'groups': scener.groups.all()
+		}
