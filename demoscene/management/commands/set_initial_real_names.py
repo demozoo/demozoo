f7a88b5f5234a54f1df8c76f2f8a09fac5a74a4c
Matt Westcott <matt@west.co.tt>
2010-08-30 22:33:59 +0000

    refine pattern matching for real names; attempt to split into first name / surname

--- demoscene/management/commands/set_initial_real_names.py
+++ demoscene/management/commands/set_initial_real_names.py
@@ -7,8 +7,20 @@ class Command(NoArgsCommand):
 		for releaser in Releaser.objects.all():
 			if releaser.first_name and releaser.surname:
 				continue
-			m = re.search(r'real name:\s*(.*?)(\.|\n)', releaser.notes, re.I)
-			if m:
-				full_name = m.group(1);
-				print "%s => %s" % (releaser.name, full_name)
+			note_match = re.search(r'real name:\s*(.*?)\s*(\n|\,|\()', releaser.notes, re.I)
+			if note_match:
+				full_name = note_match.group(1)
+				name_match = re.match(r'(.+) (van|von) (.*[^\.])', full_name, re.I)
+				if name_match:
+					first_name = name_match.group(1)
+					surname = ' '.join(name_match.group(2,3))
+				else:
+					name_match = re.match(r'(.+) (\S*[^\.])', full_name)
+					if name_match:
+						first_name = name_match.group(1)
+						surname = name_match.group(2)
+					else:
+						first_name = rstrip(full_name, '.')
+						surname = None
+				print "%s => [%s] [%s]" % (releaser.name, first_name, surname)
 
