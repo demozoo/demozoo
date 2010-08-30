814c41ad74bbc5cee282b000f306ff9ceb55a910
Matt Westcott <matt@west.co.tt>
2010-08-30 23:12:45 +0000

    catch invalid date formats in set_initial_release_dates script

--- demoscene/management/commands/set_initial_release_dates.py
+++ demoscene/management/commands/set_initial_release_dates.py
@@ -11,8 +11,11 @@ class Command(NoArgsCommand):
 				continue
 			note_match = re.search(r'release date:\s*(.*?)[\.\s]*(\n|$)', prod.notes, re.I)
 			if note_match:
-				date = FuzzyDate.parse(note_match.group(1))
-				print "%s: %s" % (prod.title, date)
+				try:
+					date = FuzzyDate.parse(note_match.group(1))
+					print "%s: %s" % (prod.title, date)
+				except ValueError:
+					raise ValueError("can't parse date %s for prod %s (%s)" % (note_match.group(1), prod.id, prod.title))
 				# prod.release_date = date
 				# prod.notes = replace(prod.notes, note_match.group(0), '')
 				# prod.save()
