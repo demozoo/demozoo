3f4590b7de5fef4dc4cd037cdb1e37f9bcd510ea
Matt Westcott <matt@west.co.tt>
2010-10-05 22:53:41 +0000

    ugly-but-functional listings of latest updates on homepage

--- demoscene/views/home.py
+++ demoscene/views/home.py
@@ -1,5 +1,12 @@
 from demoscene.shortcuts import *
+from demoscene.models import Releaser, Production
 
 def home(request):
 	return render(request, 'home.html', {
+		'latest_added_productions': Production.objects.order_by('-created_at')[0:10],
+		'latest_updated_productions': Production.objects.order_by('-updated_at')[0:10],
+		'latest_added_groups': Releaser.objects.filter(is_group = True).order_by('-created_at')[0:10],
+		'latest_updated_groups': Releaser.objects.filter(is_group = True).order_by('-updated_at')[0:10],
+		'latest_added_sceners': Releaser.objects.filter(is_group = False).order_by('-created_at')[0:10],
+		'latest_updated_sceners': Releaser.objects.filter(is_group = False).order_by('-updated_at')[0:10],
 	})
