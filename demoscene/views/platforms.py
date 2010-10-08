856e40fdb3b54fc5a450684cdf41675630e9d11d
Matt Westcott <matt@west.co.tt>
2010-10-08 22:30:53 +0000

    add notes, photos and listings for platform pages

--- demoscene/views/platforms.py
+++ demoscene/views/platforms.py
@@ -11,4 +11,6 @@ def show(request, platform_id):
 	platform = Platform.objects.get(id = platform_id)
 	return render(request, 'platforms/show.html', {
 		'platform': platform,
+		'active_groups': platform.random_active_groups()[:],
+		'productions': platform.productions.filter(release_date_date__isnull=False).order_by('-release_date_date')[0:30],
 	})
--- lib/model_thumbnail.py
+++ lib/model_thumbnail.py
@@ -1,61 +1,75 @@
 from PIL import Image
 from StringIO import StringIO
 from django.core.files.uploadedfile import SimpleUploadedFile
-import os
+from django.db import models
+import os, uuid
 
-def generate_thumbnail(original_field, thumbnail_field, size, crop = False):
-	# Set our max thumbnail size in a tuple (max width, max height)
-	thumb_width, thumb_height = size
-	aspect_ratio = float(thumb_width) / float(thumb_height)
+class ModelWithThumbnails(models.Model):
+	class Meta:
+		abstract = True
+		app_label = 'demoscene'
 	
-	original_field.seek(0)
-	# create in-memory file object because Image.open will
-	# merrily do a read() once for every format handler it has,
-	# which definitely isn't what we want if the underlying file is on S3...
-	original_file = StringIO(original_field.read())
-	image = Image.open(original_file)
-	
-	original_format = image.format
-	original_filename = os.path.basename(original_field.name)
-	original_filename_root, original_filename_ext = os.path.splitext(original_filename)
-	
-	if image.mode not in ('L', 'RGB'):
-		image = image.convert('RGB')
-	
-	if crop:
-		width, height = image.size
-		if width > height * aspect_ratio:
-			# crop left/right to achieve 4:3 aspect ratio
-			cropped_width = height * aspect_ratio
-			cropped_height = height
-		else:
-			# crop top/bottom
-			cropped_width = width
-			cropped_height = width / aspect_ratio
-		image = image.crop(
-			(
-				int((width - cropped_width) / 2),
-				int((height - cropped_height) / 2),
-				int(width - (width - cropped_width) / 2),
-				int(height - (height - cropped_height) / 2)
+	@staticmethod
+	def generate_thumbnail(original_field, thumbnail_field, size, crop = False):
+		# Set our max thumbnail size in a tuple (max width, max height)
+		thumb_width, thumb_height = size
+		aspect_ratio = float(thumb_width) / float(thumb_height)
+		
+		original_field.seek(0)
+		# create in-memory file object because Image.open will
+		# merrily do a read() once for every format handler it has,
+		# which definitely isn't what we want if the underlying file is on S3...
+		original_file = StringIO(original_field.read())
+		image = Image.open(original_file)
+		
+		original_format = image.format
+		original_filename = os.path.basename(original_field.name)
+		original_filename_root, original_filename_ext = os.path.splitext(original_filename)
+		
+		if image.mode not in ('L', 'RGB'):
+			image = image.convert('RGB')
+		
+		if crop:
+			width, height = image.size
+			if width > height * aspect_ratio:
+				# crop left/right to achieve 4:3 aspect ratio
+				cropped_width = height * aspect_ratio
+				cropped_height = height
+			else:
+				# crop top/bottom
+				cropped_width = width
+				cropped_height = width / aspect_ratio
+			image = image.crop(
+				(
+					int((width - cropped_width) / 2),
+					int((height - cropped_height) / 2),
+					int(width - (width - cropped_width) / 2),
+					int(height - (height - cropped_height) / 2)
+				)
 			)
-		)
-	image.thumbnail(size, Image.ANTIALIAS)
+		image.thumbnail(size, Image.ANTIALIAS)
+		
+		# save the thumbnail to memory
+		temp_handle = StringIO()
+		if original_format == 'JPEG':
+			image.save(temp_handle, 'jpeg')
+			new_filename = original_filename_root + '.jpg'
+			new_content_type = 'image/jpeg'
+		else:
+			image.save(temp_handle, 'png')
+			new_filename = original_filename_root + '.png'
+			new_content_type = 'image/png'
+		temp_handle.seek(0) # rewind the file
+		
+		# save to the thumbnail field
+		suf = SimpleUploadedFile(new_filename,
+			temp_handle.read(),
+			content_type = new_content_type)
+		thumbnail_field.save(suf.name, suf, save=False)
 	
-	# save the thumbnail to memory
-	temp_handle = StringIO()
-	if original_format == 'JPEG':
-		image.save(temp_handle, 'jpeg')
-		new_filename = original_filename_root + '.jpg'
-		new_content_type = 'image/jpeg'
-	else:
-		image.save(temp_handle, 'png')
-		new_filename = original_filename_root + '.png'
-		new_content_type = 'image/png'
-	temp_handle.seek(0) # rewind the file
-	
-	# save to the thumbnail field
-	suf = SimpleUploadedFile(new_filename,
-		temp_handle.read(),
-		content_type = new_content_type)
-	thumbnail_field.save(suf.name, suf, save=False)
+	@staticmethod
+	def random_path(prefix, filepath):
+		hex = uuid.uuid4().hex;
+		filename = os.path.basename(filepath)
+		filename_root, filename_ext = os.path.splitext(filename)
+		return prefix + '/' + hex[0] + '/' + hex[1] + '/' + hex[2:] + filename_ext
