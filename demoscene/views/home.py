9771a093a3fe77c8624ca8fb0d016084143b614b
Matt Westcott <matt@west.co.tt>
2010-12-12 22:16:58 +0000

    add stats to homepage

--- demoscene/views/home.py
+++ demoscene/views/home.py
@@ -9,4 +9,11 @@ def home(request):
 		'latest_updated_groups': Releaser.objects.filter(is_group = True).order_by('-updated_at')[0:10],
 		'latest_added_sceners': Releaser.objects.filter(is_group = False).order_by('-created_at')[0:10],
 		'latest_updated_sceners': Releaser.objects.filter(is_group = False).order_by('-updated_at')[0:10],
+		'stats': {
+			'production_count': Production.objects.filter(supertype = 'production').count(),
+			'graphics_count': Production.objects.filter(supertype = 'graphics').count(),
+			'music_count': Production.objects.filter(supertype = 'music').count(),
+			'scener_count': Releaser.objects.filter(is_group = False).count(),
+			'group_count': Releaser.objects.filter(is_group = False).count(),
+		}
 	})
