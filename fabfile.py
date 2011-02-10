564960832c02cce1b9336d39ee073f4ba4211188
Matt Westcott <matt@west.co.tt>
2011-02-10 22:42:58 +0000

    update fabfile to deploy as the demozoo user

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,14 @@
 from __future__ import with_statement
 from fabric.api import *
 
-env.hosts = ['root@altaria.vm.bytemark.co.uk']
+env.hosts = ['demozoo@altaria.vm.bytemark.co.uk']
 
 def deploy():
 	with cd('/var/www/demozoo2'):
 		run('svn up')
 		run('./manage.py syncdb')
 		run('./manage.py migrate')
-		run('/etc/init.d/apache2 reload')
+		run('sudo /etc/init.d/apache2 reload')
 
 def sanity():
 	with cd('/var/www/demozoo2'):
