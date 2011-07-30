27c3fceb942c2a6a880607480e5cdc5027bcd82a
Matt Westcott <matt@west.co.tt>
2011-07-30 13:07:10 +0000

    Switch to Yahoo's GeoPlanet API, which gives us more or less the same results as Placefinder but doesn't bizarrely miss out WOEIDs for certain places

--- lib/geocode.py
+++ lib/geocode.py
@@ -4,37 +4,54 @@ try:
 	import json
 except ImportError:
 	import simplejson as json
+import re
+
+strip_nonalpha = re.compile(r'[^\w\-\ \,\.]', re.UNICODE)
 
 def geocode(location):
+	# strip out punctuation other than a minimal whitelisted set, because Yahoo doesn't give us
+	# an escaping mechanism for single quotes (or has crap documentation that doesn't tell us about it)
+	location = re.sub(strip_nonalpha, '', location)
 	params = {
-		'location': location.encode('utf-8'),
-		'flags': 'J',
+		'format': 'json',
 		'appid': settings.YAHOO_API_KEY
 	}
-	url = "http://where.yahooapis.com/geocode?%s" % urllib.urlencode(params)
+	url = "http://where.yahooapis.com/v1/places.q('%s')?%s" % (urllib.quote(location), urllib.urlencode(params))
 	f = urllib2.urlopen(url)
 	response = json.load(f)
 	f.close()
 	
-	if response['ResultSet']['Found']:
-		result = response['ResultSet']['Results'][0]
-		# Return formats:
-		# country|name (e.g. 'United Kingdom', 'Europe')
-		# city|state, country|name (e.g. 'Helsinki, Finland'; 'Scotland, United Kingdom')
-		# neighborhood, city|state, country|name (e.g. 'Charlbury, Chipping Norton, United Kingdom')
-		location_parts = []
-		if result['neighborhood']:
-			location_parts.append(result['neighborhood'])
-		city_or_state = result['city'] or result['state']
-		if city_or_state:
-			location_parts.append(city_or_state)
-		location_parts.append(result['country'] or result['name'])
+	if response['places']['count']:
+		result = response['places']['place'][0]
+		
+		if result['placeTypeName attrs']['code'] == 12 or not result['country']:
+			# place is a country or international entity (continent, sea...) - just use the name
+			location = result['name']
+		elif result['placeTypeName attrs']['code'] in (10, 20, 22) and result['locality1'] and result['locality1'] != result['name']:
+			# place is a tertiary region or suburb or point of interest, with something meaningful in 'locality1' (hopefully a town name)
+			location = "%s, %s, %s" % (result['name'], result['locality1'], result['country'])
+		else:
+			# use name + country
+			location = "%s, %s" % (result['name'], result['country'])
+			
+		try:
+			latitude = result['centroid']['latitude']
+			longitude = result['centroid']['longitude']
+		except KeyError:
+			latitude = None
+			longitude = None
+		
+		try:
+			country_code = result['country attrs']['code']
+		except KeyError:
+			country_code = None
+		
 		return {
-			'location': ', '.join(location_parts),
+			'location': location,
 			'woeid': result['woeid'],
-			'latitude': result['latitude'],
-			'longitude': result['longitude'],
-			'country_code': result['countrycode'],
+			'latitude': latitude,
+			'longitude': longitude,
+			'country_code': country_code,
 		}
 	else:
 		return None
