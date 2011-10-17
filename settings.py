6a51b36cf765d2c718e3e82a5e3d5786e143d7b2
Matt Westcott <matt@west.co.tt>
2011-10-17 18:31:44 +0000

    FFS, compression is a lot borked. Abort. Abort.

--- settings.py
+++ settings.py
@@ -107,7 +107,6 @@ INSTALLED_APPS = (
 	'taggit',
 	'debug_toolbar',
 	'unjoinify',
-	'compressor',
 	
 	'demoscene',
 	'search',
@@ -132,10 +131,6 @@ DEFAULT_FILE_STORAGE = 's3boto.S3BotoStorage'
 AUTH_PROFILE_MODULE = 'demoscene.AccountProfile'
 
 INTERNAL_IPS = ('127.0.0.1',)
-
-COMPRESS = False # enable JS/CSS asset packaging/compression
-COMPRESS_URL = '/static/'
-COMPRESS_ROOT = STATICROOT
 
 # Get local settings
 try:
