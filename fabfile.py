d0f593d78f9d2d37fe75755bccf324cd43b72f61
Matt Westcott <matt@west.co.tt>
2011-11-29 01:21:54 +0000

    perhaps activating virtualenv and running pip install in one line will work better

--- fabfile.py
+++ fabfile.py
@@ -6,8 +6,7 @@ env.hosts = ['demozoo@altaria.vm.bytemark.co.uk']
 def deploy():
 	with cd('/var/www/demozoo2'):
 		run('svn up')
-		run('source /home/demozoo/virtualenv/bin/activate')
-		run('pip install -E /home/demozoo/virtualenv/ -r /var/www/demozoo2/requirements.txt')
+		run('source /home/demozoo/virtualenv/bin/activate && pip install -E /home/demozoo/virtualenv/ -r /var/www/demozoo2/requirements.txt')
 		run('./manage.py syncdb')
 		run('./manage.py migrate')
 		run('sudo /etc/init.d/apache2 reload')
