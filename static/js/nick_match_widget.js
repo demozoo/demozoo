949b4b97debda695f11943c0141b8502daaa44ed
Matt Westcott <matt@west.co.tt>
2012-01-10 21:44:54 +0000

    change 'input[checked]' tests to 'input:checked' in nick_match_widget.js to be in line with jquery 1.6 http://api.jquery.com/prop/ - i.e. check the current property state, not the attribute

--- static/js/nick_match_widget.js
+++ static/js/nick_match_widget.js
@@ -102,7 +102,7 @@ function NickMatchWidget(elem, nickSelection, choices, fieldPrefix) {
 	function copyIconFromSelectedLi() {
 		/* inherit icon for selectedResult from the li with the selected radio button,
 		or 'error' icon if there is none */
-		var selectedLi = $('li:has(input[checked])', suggestionsUl);
+		var selectedLi = $('li:has(input:checked)', suggestionsUl);
 		if (selectedLi.length) {
 			var classNames = 'selected_result ' + selectedLi.attr('class');
 			if (selectedResult.hasClass('active')) classNames += ' active';
@@ -116,14 +116,15 @@ function NickMatchWidget(elem, nickSelection, choices, fieldPrefix) {
 	copyIconFromSelectedLi();
 	
 	function highlightSelectedLi() {
+		console.log($('li', suggestionsUl));
 		$('li', suggestionsUl).removeClass('selected');
-		$('li:has(input[checked])', suggestionsUl).addClass('selected');
+		$('li:has(input:checked)', suggestionsUl).addClass('selected');
 	}
 	
 	function keypress(e) {
 		if (e.which == 40) { /* cursor down */
 			if (suggestionsUl.is(':visible')) {
-				var nextElem = $('li:has(input[checked])', suggestionsUl).next();
+				var nextElem = $('li:has(input:checked)', suggestionsUl).next();
 				if (nextElem.length) {
 					$('input', nextElem).attr('checked', 'checked');
 				} else {
@@ -138,7 +139,7 @@ function NickMatchWidget(elem, nickSelection, choices, fieldPrefix) {
 		} else if (e.which == 38) { /* cursor up */
 			if (suggestionsUl.is(':visible')) {
 				suggestionsUl.show();
-				var prevElem = $('li:has(input[checked])', suggestionsUl).prev();
+				var prevElem = $('li:has(input:checked)', suggestionsUl).prev();
 				if (prevElem.length) {
 					$('input', prevElem).attr('checked', 'checked');
 				} else {
