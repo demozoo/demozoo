a0d627b80b4eccbfaebad56ef6f15fea3a0a71d3
Matt Westcott <matt@west.co.tt>
2013-02-06 01:01:45 +0000

    add maintenance command + scheduled task to rebuild thumbnails; also, better display of thumbnails across screenshot maintenance page and party series page

--- screenshots/management/commands/rebuild_screenshots.py
+++ screenshots/management/commands/rebuild_screenshots.py
@@ -1,14 +1,11 @@
 from django.core.management.base import NoArgsCommand
 from demoscene.models import Screenshot
-from model_thumbnail import generate_thumbnail
+from screenshots.tasks import rebuild_screenshot
+
 
 class Command(NoArgsCommand):
 	def handle_noargs(self, **options):
 		screens = Screenshot.objects.all()
 		for screen in screens:
 			print "rebuilding %s" % screen
-			try:
-				generate_thumbnail(screen.original, screen.thumbnail, (135, 90), crop = True)
-				screen.save()
-			except AttributeError: # thrown on broken image link
-				pass
+			rebuild_screenshot.delay(screen.id)
--- screenshots/tasks.py
+++ screenshots/tasks.py
@@ -3,6 +3,8 @@ import os
 import errno
 import re
 import uuid
+import urllib2
+import cStringIO
 
 from demoscene.models import Screenshot
 from screenshots.models import PILConvertibleImage
@@ -20,6 +22,29 @@ except OSError as exc:
 		raise
 
 
+def create_basename(screenshot_id):
+		u = uuid.uuid4().hex
+		return u[0:2] + '/' + u[2:4] + '/' + u[4:8] + '.' + str(screenshot_id) + '.'
+
+
+def upload_original(img, screenshot, basename, reduced_redundancy=False):
+	orig, orig_size, orig_format = img.create_original()
+	screenshot.original_url = upload_to_s3(orig, 'screens/o/' + basename + orig_format, orig_format, reduced_redundancy=reduced_redundancy)
+	screenshot.original_width, screenshot.original_height = orig_size
+
+
+def upload_standard(img, screenshot, basename):  # always reduced redundancy
+	standard, standard_size, standard_format = img.create_thumbnail((400, 300))
+	screenshot.standard_url = upload_to_s3(standard, 'screens/s/' + basename + standard_format, standard_format, reduced_redundancy=True)
+	screenshot.standard_width, screenshot.standard_height = standard_size
+
+
+def upload_thumb(img, screenshot, basename):  # always reduced redundancy
+	thumb, thumb_size, thumb_format = img.create_thumbnail((200, 150))
+	screenshot.thumbnail_url = upload_to_s3(thumb, 'screens/t/' + basename + thumb_format, thumb_format, reduced_redundancy=True)
+	screenshot.thumbnail_width, screenshot.thumbnail_height = thumb_size
+
+
 @task(ignore_result=True)
 def create_screenshot_versions_from_local_file(screenshot_id, filename):
 	try:
@@ -27,21 +52,10 @@ def create_screenshot_versions_from_local_file(screenshot_id, filename):
 		f = open(filename, 'rb')
 		img = PILConvertibleImage(f)
 
-		u = uuid.uuid4().hex
-		basename = u[0:2] + '/' + u[2:4] + '/' + u[4:8] + '.' + str(screenshot_id) + '.'
-
-		orig, orig_size, orig_format = img.create_original()
-		screenshot.original_url = upload_to_s3(orig, 'screens/o/' + basename + orig_format, orig_format)
-		screenshot.original_width, screenshot.original_height = orig_size
-
-		standard, standard_size, standard_format = img.create_thumbnail((400, 300))
-		screenshot.standard_url = upload_to_s3(standard, 'screens/s/' + basename + standard_format, standard_format, reduced_redundancy=True)
-		screenshot.standard_width, screenshot.standard_height = standard_size
-
-		thumb, thumb_size, thumb_format = img.create_thumbnail((200, 150))
-		screenshot.thumbnail_url = upload_to_s3(thumb, 'screens/t/' + basename + thumb_format, thumb_format, reduced_redundancy=True)
-		screenshot.thumbnail_width, screenshot.thumbnail_height = thumb_size
-
+		basename = create_basename(screenshot_id)
+		upload_original(img, screenshot, basename)
+		upload_standard(img, screenshot, basename)
+		upload_thumb(img, screenshot, basename)
 		screenshot.save()
 
 		f.close()
@@ -51,6 +65,29 @@ def create_screenshot_versions_from_local_file(screenshot_id, filename):
 		pass
 
 	os.remove(filename)
+
+
+@task(ignore_result=True)
+def rebuild_screenshot(screenshot_id):
+	try:
+		screenshot = Screenshot.objects.get(id=screenshot_id)
+		f = urllib2.urlopen(screenshot.original_url, None, 10)
+		# read into a cStringIO buffer so that PIL can seek on it (which isn't possible for urllib2 responses) -
+		# see http://mail.python.org/pipermail/image-sig/2004-April/002729.html
+		buf = cStringIO.StringIO(f.read())
+		img = PILConvertibleImage(buf)
+
+		basename = create_basename(screenshot_id)
+		upload_original(img, screenshot, basename)
+		upload_standard(img, screenshot, basename)
+		upload_thumb(img, screenshot, basename)
+		screenshot.save()
+
+		f.close()
+
+	except Screenshot.DoesNotExist:
+		# guess it was deleted in the meantime, then.
+		pass
 
 
 def capture_upload_for_processing(uploaded_file, screenshot_id):
