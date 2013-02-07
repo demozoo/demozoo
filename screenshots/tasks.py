a61d52eb9a1644c8fc41a7a3eb51de7da09308c6
Matt Westcott <matt@west.co.tt>
2013-02-07 22:27:08 +0000

    add a token rate limit on rebuild_screenshot so that new uploads from local files get priority

--- screenshots/tasks.py
+++ screenshots/tasks.py
@@ -67,7 +67,8 @@ def create_screenshot_versions_from_local_file(screenshot_id, filename):
 	os.remove(filename)
 
 
-@task(ignore_result=True)
+# token rate limit so that new uploads from local files get priority
+@task(rate_limit='12/m', ignore_result=True)
 def rebuild_screenshot(screenshot_id):
 	try:
 		screenshot = Screenshot.objects.get(id=screenshot_id)
