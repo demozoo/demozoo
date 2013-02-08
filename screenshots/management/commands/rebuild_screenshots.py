561f05f670f5574c52f43240f1e762606ee71ddb
Matt Westcott <matt@west.co.tt>
2013-02-08 21:43:02 +0000

    tweak rebuild_screenshots task to only rebuild ones that still live under media.demozoo.org/screenshots/

--- screenshots/management/commands/rebuild_screenshots.py
+++ screenshots/management/commands/rebuild_screenshots.py
@@ -2,10 +2,13 @@ from django.core.management.base import NoArgsCommand
 from demoscene.models import Screenshot
 from screenshots.tasks import rebuild_screenshot
 
+# Recreate and re-upload files for screenshots that are still in the original format/location,
+# as indicated by having URLs under media.demozoo.org/screenshots/ (not /screens/)
+
 
 class Command(NoArgsCommand):
 	def handle_noargs(self, **options):
-		screens = Screenshot.objects.all()
+		screens = Screenshot.objects.filter(original_url__contains='/screenshots/')
 		for screen in screens:
 			print "rebuilding %s" % screen
 			rebuild_screenshot.delay(screen.id)
