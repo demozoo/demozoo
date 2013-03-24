5520c62bca06d545dc980a0d41f8472677ea598d
Matt Westcott <matt@west.co.tt>
2013-03-24 04:56:56 +0000

    add models and admin interface for homepage banner and teaser

---
+++ homepage/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from homepage.models import *
+
+
+admin.site.register(Banner, list_display=('title', 'show_for_anonymous_users', 'show_for_logged_in_users', 'updated_at'))
+admin.site.register(Teaser, list_display=('title', 'show_for_anonymous_users', 'show_for_logged_in_users', 'updated_at'))
--- homepage/models.py
+++ homepage/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from demoscene.models import random_path
+
+class Banner(models.Model):
+	image = models.ImageField(
+		upload_to=(lambda i, f: random_path('homepage_banners', f)),
+		width_field='image_width', height_field='image_height')
+	image_width = models.IntegerField(editable=False)
+	image_height = models.IntegerField(editable=False)
+	title = models.CharField(max_length=255)
+	text = models.TextField(blank=True)
+	url = models.CharField(max_length=255)
+
+	show_for_anonymous_users = models.BooleanField(default=True)
+	show_for_logged_in_users = models.BooleanField(default=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return self.title
+
+class Teaser(models.Model):
+	image = models.ImageField(
+		upload_to=(lambda i, f: random_path('homepage_teasers', f)),
+		width_field='image_width', height_field='image_height')
+	image_width = models.IntegerField(editable=False)
+	image_height = models.IntegerField(editable=False)
+	title = models.CharField(max_length=255)
+	text = models.TextField(blank=True)
+	url = models.CharField(max_length=255)
+
+	show_for_anonymous_users = models.BooleanField(default=True)
+	show_for_logged_in_users = models.BooleanField(default=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __unicode__(self):
+		return self.title
