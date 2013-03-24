1845b9f98cf1afacd2f624301b0348e32b1f19f4
Matt Westcott <matt@west.co.tt>
2013-03-24 05:25:55 +0000

    add banner to homepage template

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from homepage.models import Banner
+
 def home(request):
+	if request.user.is_authenticated():
+		banners = Banner.objects.filter(show_for_logged_in_users=True).order_by('created_at')[:1]
+	else:
+		banners = Banner.objects.filter(show_for_anonymous_users=True).order_by('created_at')[:1]
+
+	try:
+		banner = banners[0]
+	except IndexError:
+		banner = None
+
 	return render(request, 'home.html', {
+		'banner': banner
 	})
