6e82b86377a84167d1d0121c173f9cd2858d3ba8
Matt Westcott <matt@west.co.tt>
2013-03-24 21:35:14 +0000

    add homepage teasers

--- homepage/admin.py
+++ homepage/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 from homepage.models import *
 
 
-admin.site.register(Banner, list_display=('title', 'show_for_anonymous_users', 'show_for_logged_in_users', 'updated_at'))
-admin.site.register(Teaser, list_display=('title', 'show_for_anonymous_users', 'show_for_logged_in_users', 'updated_at'))
+admin.site.register(Banner, list_display=('title', 'show_for_anonymous_users', 'show_for_logged_in_users', 'created_at'))
+admin.site.register(Teaser, list_display=('title', 'show_for_anonymous_users', 'show_for_logged_in_users', 'created_at'))
--- homepage/views.py
+++ homepage/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 
-from homepage.models import Banner
+from homepage.models import Banner, Teaser
 
 def home(request):
 	if request.user.is_authenticated():
 		banners = Banner.objects.filter(show_for_logged_in_users=True).order_by('created_at')[:1]
+		teasers = Teaser.objects.filter(show_for_logged_in_users=True).order_by('created_at')[:3]
 	else:
 		banners = Banner.objects.filter(show_for_anonymous_users=True).order_by('created_at')[:1]
+		teasers = Teaser.objects.filter(show_for_anonymous_users=True).order_by('created_at')[:3]
 
 	try:
 		banner = banners[0]
@@ -14,5 +16,6 @@ def home(request):
 		banner = None
 
 	return render(request, 'home.html', {
-		'banner': banner
+		'banner': banner,
+		'teasers': teasers,
 	})
