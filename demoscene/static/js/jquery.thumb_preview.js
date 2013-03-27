a7328fdd7855c67861491ef606bf5a2a863e83a2
Matt Westcott <matt@west.co.tt>
2013-03-27 22:32:28 +0100

    make thumbnail popups as big as we can. No reason not to...

--- demoscene/static/js/jquery.thumb_preview.js
+++ demoscene/static/js/jquery.thumb_preview.js
@@ -1,6 +1,9 @@
 (function($) {
 	var popup;
 
+	var WIDTH = 200;
+	var HEIGHT = 150;
+
 	$.fn.thumbPreview = function() {
 		$(this).hover(function() {
 			if (!popup) {
@@ -8,22 +11,22 @@
 				$('body').append(popup);
 				popup.css({
 					'position': 'absolute', 'z-index': 100,
-					'height': '100px', 'width': '133px', 'margin-top': '-125px', 'margin-left': '-76px',
+					'height': HEIGHT + 'px', 'width': WIDTH + 'px', 'margin-top': -(HEIGHT + 25) + 'px', 'margin-left': -(WIDTH / 2) + 'px',
 					'padding': '10px',
-					'line-height': '100px', 'text-align': 'center',
+					'line-height': HEIGHT + 'px', 'text-align': 'center',
 					'background-color': '#eee', 'border': '1px solid #ccc', 'border-radius': '4px'
 				});
 				popup.find('img').css({'vertical-align': 'middle'});
 			}
 			var thumbPos = $(this).offset();
 			var thumbImg = $(this).find('img');
-			var left = Math.max(thumbPos.left + $(this).width() / 2, 76);
+			var left = Math.max(thumbPos.left + $(this).width() / 2, WIDTH / 2);
 
 			var thumbWidth = thumbImg.data('natural-width');
 			var thumbHeight = thumbImg.data('natural-height');
-			/* scale down by whatever factor is required to get both width and height within 133x100 */
-			var widthScale = Math.min(133 / thumbWidth, 1);
-			var heightScale = Math.min(100 / thumbHeight, 1);
+			/* scale down by whatever factor is required to get both width and height within the bounding box */
+			var widthScale = Math.min(WIDTH / thumbWidth, 1);
+			var heightScale = Math.min(HEIGHT / thumbHeight, 1);
 			var scale = Math.min(widthScale, heightScale);
 
 			width = Math.floor(thumbWidth * scale);
