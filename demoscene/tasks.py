a6a60027cdebae0a6528f465956b2b0076fb1a94
Matt Westcott <matt@west.co.tt>
2013-03-27 14:46:07 +0100

    fix set_default_screenshots task so that it only selects screenshots that have completed processing

--- demoscene/tasks.py
+++ demoscene/tasks.py
@@ -18,15 +18,15 @@ def set_default_screenshots():
 	cur = connection.cursor()
 	cur2 = connection.cursor()
 
-	# unset default_screenshot for productions that don't have any screenshots
+	# unset default_screenshot for productions that don't have any processed screenshots
 	# (if things are working properly then we would expect this to be redundant. But no harm in being thorough :-) )
 	cur.execute('''
 		UPDATE demoscene_production SET default_screenshot_id = NULL
-		WHERE id NOT IN (SELECT DISTINCT production_id FROM demoscene_screenshot)
+		WHERE id NOT IN (SELECT DISTINCT production_id FROM demoscene_screenshot WHERE thumbnail_url <> '')
 	''')
 
-	# get all screenshots, group by production_id
-	cur.execute("SELECT id, production_id FROM demoscene_screenshot ORDER BY production_id")
+	# get all processed screenshots (= ones that have thumbnails), group by production_id
+	cur.execute("SELECT id, production_id FROM demoscene_screenshot WHERE thumbnail_url <> '' ORDER BY production_id")
 	for production_id, screenshots in groupby(cur, lambda row: row[1]):
 		# select a random screenshot from the group and set it as default_screenshot for that prod
 		screenshot_id = random.choice([row[0] for row in screenshots])
