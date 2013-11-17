747fd94e8ae2df3898b2c2bf1e33065c25dc3ecd
Matt Westcott <matt@west.co.tt>
2013-11-17 16:19:09 +0000

    import level 2 admin codes

--- geonameslite/management/commands/load_geonames.py
+++ geonameslite/management/commands/load_geonames.py
@@ -2,7 +2,7 @@ from django.core.management.base import NoArgsCommand
 
 import urllib2
 
-from geonameslite.models import Country, Admin1Code
+from geonameslite.models import Country, Admin1Code, Admin2Code
 
 
 class Command(NoArgsCommand):
@@ -12,6 +12,7 @@ class Command(NoArgsCommand):
 		print "Clearing tables"
 		Country.objects.all().delete()
 		Admin1Code.objects.all().delete()
+		Admin2Code.objects.all().delete()
 
 	def load_countries(self):
 		print "Loading countries"
@@ -47,7 +48,51 @@ class Command(NoArgsCommand):
 		fd.close()
 		Admin1Code.objects.bulk_create(objects)
 
+	def load_admin2_codes(self):
+		print "Loading admin level 2 codes"
+		objects = []
+		admin2_list = []  # to find duplicated
+		skipped_duplicated = 0
+		fd = urllib2.urlopen('http://download.geonames.org/export/dump/admin2Codes.txt')
+		for line in fd:
+			fields = line[:-1].split('\t')
+			codes, name = fields[0:2]
+			country_code, admin1_code, admin2_code = codes.split('.')
+
+			# if there is a duplicated
+			long_code = "{}.{}.{}".format(country_code, admin1_code, name)
+			if long_code in admin2_list:
+				skipped_duplicated += 1
+				continue
+
+			admin2_list.append(long_code)
+
+			geonameid = fields[3]
+			admin1_dic = self.countries[country_code].get(admin1_code)
+
+			# if there is not admin1 level we save it but we don't keep it for the localities
+			if admin1_dic is None:
+				admin1_id = None
+			else:
+				# If not, we get the id of admin1 and we save geonameid for filling in Localities later
+				admin1_id = admin1_dic['geonameid']
+				admin1_dic['admins2'][admin2_code] = geonameid
+
+			name = unicode(name, 'utf-8')
+			objects.append(Admin2Code(geonameid=geonameid,
+									  code=admin2_code,
+									  name=name,
+									  country_id=country_code,
+									  admin1_id=admin1_id))
+
+		fd.close()
+
+		Admin2Code.objects.bulk_create(objects)
+		print '{0:8d} Admin2Codes loaded'.format(Admin2Code.objects.all().count())
+		print '{0:8d} Admin2Codes skipped because duplicated'.format(skipped_duplicated)
+
 	def handle_noargs(self, **options):
 		self.clear_tables()
 		self.load_countries()
 		self.load_admin1_codes()
+		self.load_admin2_codes()
--- geonameslite/models.py
+++ geonameslite/models.py
@@ -17,3 +17,21 @@ class Admin1Code(models.Model):
 
 	def __unicode__(self):
 		return u'%s, %s' % (self.name, self.country.name)
+
+
+class Admin2Code(models.Model):
+	geonameid = models.PositiveIntegerField(primary_key=True)
+	code = models.CharField(max_length=30)
+	name = models.CharField(max_length=200)
+	country = models.ForeignKey(Country, related_name="admins2")
+	admin1 = models.ForeignKey(Admin1Code, null=True, blank=True, related_name="admins2")
+
+	class Meta:
+		unique_together = (("country", "admin1", "name"),)
+
+	def __unicode__(self):
+		s = u"{}".format(self.name)
+		if self.admin1 is not None:
+			s = u"{}, {}".format(s, self.admin1.name)
+
+		return u"{}, {}".format(s, self.country.name)
