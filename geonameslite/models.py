2ba1dbb1d2d1d853750e19e41b213c8fa0a28e1e
Matt Westcott <matt@west.co.tt>
2013-11-22 20:01:43 +0000

    Set up non-broken indexes at syncdb time, and implement a passable Locality.search method

--- geonameslite/models.py
+++ geonameslite/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.db.models import Q
+
+import re
 
 
 class Country(models.Model):
 	code = models.CharField(max_length=2, primary_key=True)
-	name = models.CharField(max_length=200, unique=True, db_index=True)
+	name = models.CharField(max_length=200, unique=True)
 
 	def __unicode__(self):
 		return self.name
@@ -39,7 +42,7 @@ class Admin2Code(models.Model):
 
 class Locality(models.Model):
 	geonameid = models.PositiveIntegerField(primary_key=True)
-	name = models.CharField(max_length=200, db_index=True)
+	name = models.CharField(max_length=200)
 	country = models.ForeignKey(Country, null=True, blank=True, related_name="localities")
 	admin1 = models.ForeignKey(Admin1Code, null=True, blank=True, related_name="localities")
 	admin2 = models.ForeignKey(Admin2Code, null=True, blank=True, related_name="localities")
@@ -59,9 +62,42 @@ class Locality(models.Model):
 
 		return long_name
 
+	@staticmethod
+	def search(term, partial=False):
+		term = term.strip()
+		if not term:
+			return Locality.objects.none()
+
+		parts = re.split(r',\s*', term)
+
+		if len(parts) == 1 and partial:
+			alt_name_matches = list(AlternateName.objects.filter(name__istartswith=parts[0]).values_list('locality_id', flat=True).distinct())
+			query = Q(name__istartswith=parts[0]) | Q(geonameid__in=alt_name_matches)
+		else:
+			alt_name_matches = list(AlternateName.objects.filter(name__iexact=parts[0]).values_list('locality_id', flat=True).distinct())
+			query = Q(name__iexact=parts[0]) | Q(geonameid__in=alt_name_matches)
+			if partial:
+				qualifiers = parts[1:-1]
+				query &= (
+					Q(country__name__istartswith=parts[-1]) | Q(country__code__istartswith=parts[-1])
+					| Q(admin1__name__istartswith=parts[-1])
+					| Q(admin2__name__istartswith=parts[-1])
+				)
+			else:
+				qualifiers = parts[1:]
+
+			for qualifier in qualifiers:
+				query &= (
+					Q(country__name__iexact=parts[-1]) | Q(country__code__iexact=parts[-1])
+					| Q(admin1__name__iexact=parts[-1])
+					| Q(admin2__name__iexact=parts[-1])
+				)
+
+		return Locality.objects.filter(query).order_by('-population')
+
 class AlternateName(models.Model):
 	locality = models.ForeignKey(Locality, related_name="alternatenames")
-	name = models.CharField(max_length=200, db_index=True)
+	name = models.CharField(max_length=200)
 
 	def __unicode__(self):
 		return self.name
