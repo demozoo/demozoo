fe621bc42a39380eb463d1de7040a65a78f9105d
Matt Westcott <matt@west.co.tt>
2013-12-02 17:52:05 +0100

    restructure EDIT_DESCRIPTION_FORMATTERS to support outputting log messages as HTML

--- editlog/models.py
+++ editlog/models.py
@@ -1,30 +1,75 @@
 import datetime
 
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+from django.utils.html import escape, conditional_escape
+from django.utils.safestring import mark_safe
 
 
 def releaser_for_edit_description(self):
 	for item in self.edited_items.all():
 		if item.role in ('scener', 'group', 'releaser'):
-			return "%s %s" % (item.role, item.name)
+			return item
 
 EDIT_DESCRIPTION_FORMATTERS = {
-	'add_nick': lambda self: "Added %s as a nick for %s" % (self.detail, releaser_for_edit_description(self)),
-	'create_group': lambda self: "Added group %s" % (self.get_item('group').name),
-	'create_production': lambda self: "Added production %s" % (self.get_item('production').name),
-	'create_scener': lambda self: "Added scener %s" % (self.get_item('scener').name),
-	'edit_party_notes': lambda self: "Edited notes for party %s" % (self.get_item('party').name),
-	'edit_production_core_details': lambda self: "Edited core details of %s: %s" % (self.get_item('production').name, self.detail),
-	'edit_production_notes': lambda self: "Edited notes for production %s" % (self.get_item('production').name),
-	'edit_releaser_notes': lambda self: "Edited notes for %s" % releaser_for_edit_description(self),
-	'edit_scener_location': lambda self: "Set %s's location to %s" % (self.get_item('scener').name, self.detail),
-	'edit_scener_real_name': lambda self: ("Set %s's real name" % (self.get_item('scener').name)) if self.detail == 'set_real_name' else ("Updated visibility of %s's real name" % (self.get_item('scener').name)),
-	'party_edit_external_links': lambda self: "%s on party %s" % (self.detail, self.get_item('party').name),
-	'production_edit_download_links': lambda self: "%s on production %s" % (self.detail, self.get_item('production').name),
-	'production_edit_external_links': lambda self: "%s on production %s" % (self.detail, self.get_item('production').name),
-	'releaser_edit_external_links': lambda self: "%s on %s" % (self.detail, releaser_for_edit_description(self)),
+	'add_nick': (
+		"Added %(nick)s as a nick for %(releaser)s",
+		lambda self: {'nick': self.detail, 'releaser': releaser_for_edit_description(self)}
+	),
+	'create_group': (
+		"Added group %(group)s",
+		lambda self: {'group': self.get_item('group')}
+	),
+	'create_production': (
+		 "Added production %(production)s",
+		lambda self: {'production': self.get_item('production')}
+	),
+	'create_scener': (
+		"Added scener %(scener)s",
+		lambda self: {'scener': self.get_item('scener')}
+	),
+	'edit_party_notes': (
+		"Edited notes for party %s",
+		lambda self: {'party': self.get_item('party')}
+	),
+	'edit_production_core_details': (
+		"Edited core details of %(production)s: %(detail)s",
+		lambda self: {'production': self.get_item('production'), 'detail': self.detail}
+	),
+	'edit_production_notes': (
+		"Edited notes for production %(production)s",
+		lambda self: {'production': self.get_item('production')}
+	),
+	'edit_releaser_notes': (
+		"Edited notes for %(releaser)s",
+		lambda self: {'releaser': releaser_for_edit_description(self)}
+	),
+	'edit_scener_location': (
+		"Set %(scener)s's location to %(location)s",
+		lambda self: {'scener': self.get_item('scener'), 'location': self.detail}
+	),
+	'edit_scener_real_name': lambda self: (
+		("Set %(scener)s's real name", lambda self: {'scener': self.get_item('scener')}) if self.detail == 'set_real_name'
+		else ("Updated visibility of %(scener)s's real name", lambda self: {'scener': self.get_item('scener')})
+	),
+	'party_edit_external_links': (
+		"%(detail)s on party %(party)s",
+		lambda self: {'detail': self.detail, 'party': self.get_item('party')}
+	),
+	'production_edit_download_links': (
+		"%(detail)s on production %(production)s",
+		lambda self: {'detail': self.detail, 'production': self.get_item('production')}
+	),
+	'production_edit_external_links': (
+		"%(detail)s on production %(production)s",
+		lambda self: {'detail': self.detail, 'production': self.get_item('production')}
+	),
+	'releaser_edit_external_links': (
+		"%(detail)s on %(releaser)s",
+		lambda self: {'detail': self.detail, 'releaser': releaser_for_edit_description(self)}
+	),
 }
 
 
@@ -46,11 +91,38 @@ class Edit(models.Model):
 		super(Edit, self).save(*args, **kwargs)
 
 	def __unicode__(self):
-		formatter = EDIT_DESCRIPTION_FORMATTERS.get(self.action_type)
-		if formatter:
-			return formatter(self)
-		else:
+		try:
+			formatter = EDIT_DESCRIPTION_FORMATTERS[self.action_type]
+		except KeyError:
 			return self.action_type
+
+		if callable(formatter):
+			pattern, params_fn = formatter(self)
+		else:
+			pattern, params_fn = formatter
+
+		params = params_fn(self)
+		return pattern % params
+
+	def html_description(self):
+		try:
+			formatter = EDIT_DESCRIPTION_FORMATTERS[self.action_type]
+		except KeyError:
+			return self.action_type
+
+		if callable(formatter):
+			pattern, params_fn = formatter(self)
+		else:
+			pattern, params_fn = formatter
+
+		params = params_fn(self)
+		for key, val in params.iteritems():
+			try:
+				params[key] = conditional_escape(val.html_name())
+			except AttributeError:
+				params[key] = conditional_escape(val)
+
+		return mark_safe(pattern % params)
 
 
 class EditedItem(models.Model):
@@ -60,3 +132,22 @@ class EditedItem(models.Model):
 	item_id = models.PositiveIntegerField()
 	item = generic.GenericForeignKey('item_content_type', 'item_id')
 	name = models.CharField(max_length=255, blank=True)
+
+	def __unicode__(self):
+		return self.name
+
+	def html_name(self):
+		url = None
+		if self.role in ('production', 'soundtrack'):
+			url = reverse('production', args=[self.item_id])
+		elif self.role == 'party':
+			url = reverse('party', args=[self.item_id])
+		elif self.role in ('scener', 'member'):
+			url = reverse('scener', args=[self.item_id])
+		elif self.role in ('group', 'subgroup', 'supergroup'):
+			url = reverse('group', args=[self.item_id])
+
+		if url:
+			return mark_safe('<a href="%s">%s</a>' % (escape(url), escape(self.name)))
+		else:
+			return mark_safe(escape(self.name))
