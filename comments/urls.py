94acb98a12ef23eff954aaeac04def95c268571d
Matt Westcott <matt@west.co.tt>
2013-12-19 00:10:41 +0000

    add 'edit comment' option for admins

--- comments/urls.py
+++ comments/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import *
 
 urlpatterns = patterns('comments.views',
 	(r'^productions/(\d+)/comments/new/$', 'add_production_comment', {}, 'add_production_comment'),
+	(r'^productions/(\d+)/comments/(\d+)/edit/$', 'edit_production_comment', {}, 'edit_production_comment'),
 )
--- comments/views.py
+++ comments/views.py
@@ -22,3 +22,25 @@ def add_production_comment(request, production_id):
 		'production': production,
 		'comment_form': form,
 	})
+
+@login_required
+def edit_production_comment(request, production_id, comment_id):
+	production = get_object_or_404(Production, id=production_id)
+	comment = get_object_or_404(ProductionComment, id=comment_id, production=production)
+
+	if not request.user.is_staff:
+		return redirect(production.get_absolute_url() + ('#comment-%d' % comment.id))
+
+	if request.POST:
+		form = ProductionCommentForm(request.POST, instance=comment, prefix='comment')
+		if form.is_valid():
+			form.save()
+			return redirect(production.get_absolute_url() + ('#comment-%d' % comment.id))
+	else:
+		form = ProductionCommentForm(instance=comment, prefix='comment')
+
+	return render(request, 'comments/edit_production_comment.html', {
+		'production': production,
+		'comment': comment,
+		'comment_form': form,
+	})
