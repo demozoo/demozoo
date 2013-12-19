0743a86798f30df02f0fa7d2c212cc4d33ef4175
Matt Westcott <matt@west.co.tt>
2013-12-19 00:15:09 +0000

    make comments behave correctly in read-only mode

--- comments/views.py
+++ comments/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 
+from read_only_mode import writeable_site_required
+
 from comments.models import ProductionComment
 from comments.forms import ProductionCommentForm
 from demoscene.models import Production
 
+@writeable_site_required
 @login_required
 def add_production_comment(request, production_id):
 	production = get_object_or_404(Production, id=production_id)
@@ -23,6 +26,7 @@ def add_production_comment(request, production_id):
 		'comment_form': form,
 	})
 
+@writeable_site_required
 @login_required
 def edit_production_comment(request, production_id, comment_id):
 	production = get_object_or_404(Production, id=production_id)
