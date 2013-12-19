1812a99c26d9bec3dcf148044cc9cae81f73951e
Matt Westcott <matt@west.co.tt>
2013-12-19 00:19:42 +0000

    implement comment deletion for admins

--- comments/urls.py
+++ comments/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import *
 urlpatterns = patterns('comments.views',
 	(r'^productions/(\d+)/comments/new/$', 'add_production_comment', {}, 'add_production_comment'),
 	(r'^productions/(\d+)/comments/(\d+)/edit/$', 'edit_production_comment', {}, 'edit_production_comment'),
+	(r'^productions/(\d+)/comments/(\d+)/delete/$', 'delete_production_comment', {}, 'delete_production_comment'),
 )
--- comments/views.py
+++ comments/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 
 from read_only_mode import writeable_site_required
 
 from comments.models import ProductionComment
 from comments.forms import ProductionCommentForm
 from demoscene.models import Production
+from demoscene.shortcuts import simple_ajax_confirmation
 
 @writeable_site_required
 @login_required
@@ -48,3 +50,22 @@ def edit_production_comment(request, production_id, comment_id):
 		'comment': comment,
 		'comment_form': form,
 	})
+
+@writeable_site_required
+@login_required
+def delete_production_comment(request, production_id, comment_id):
+	production = get_object_or_404(Production, id=production_id)
+	comment = get_object_or_404(ProductionComment, id=comment_id, production=production)
+
+	if not request.user.is_staff:
+		return redirect(production.get_absolute_url() + ('#comment-%d' % comment.id))
+
+	if request.method == 'POST':
+		if request.POST.get('yes'):
+			comment.delete()
+		return redirect(production.get_absolute_url())
+	else:
+		return simple_ajax_confirmation(request,
+			reverse('delete_production_comment', args=[production_id, comment_id]),
+			"Are you sure you want to delete this comment?",
+			html_title="Deleting comment on %s" % production.title)
