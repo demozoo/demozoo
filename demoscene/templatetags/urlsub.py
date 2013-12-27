9ffed4aef0150a4347ca60db71fc1d0a14d4143b
Matt Westcott <matt@west.co.tt>
2013-12-27 01:20:39 +0000

    Neaten up querystring munging for pagination / sorting links using new urlsub template tag, and ditch the rather broken query_string tag

---
+++ demoscene/templatetags/urlsub.py
@@ -0,0 +1,19 @@
+from django import template
+from django.utils.html import conditional_escape
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def urlsub(context, **params_to_replace):
+	params = context['request'].GET.copy()
+	for k, v in params_to_replace.iteritems():
+		# can't use params.update here, because with QueryDict that appends to the list
+		# for each param rather than updating it. Grr.
+		params[k] = v
+
+	param_string = params.urlencode()
+
+	if param_string:
+		return '?' + conditional_escape(param_string)
+	else:
+		return ''
