fcbb7d5386993a49f0dded2e61902d477bcc1ecb
Matt Westcott <matt@west.co.tt>
2014-02-19 19:20:34 +0000

    remove all editorial content from the not-a-homepage. This is really dumb.

--- homepage/views.py
+++ homepage/views.py
@@ -25,21 +25,9 @@ def home(request):
 
 
 def not_the_homepage(request):
-	if request.user.is_authenticated():
-		banners = Banner.objects.filter(show_for_logged_in_users=True).order_by('-created_at')[:1]
-		teasers = Teaser.objects.filter(show_for_logged_in_users=True).order_by('-created_at')[:3]
-	else:
-		banners = Banner.objects.filter(show_for_anonymous_users=True).order_by('-created_at')[:1]
-		teasers = Teaser.objects.filter(show_for_anonymous_users=True).order_by('-created_at')[:3]
-
-	try:
-		banner = banners[0]
-	except IndexError:
-		banner = None
-
 	return render(request, 'home.html', {
-		'banner': banner,
-		'teasers': teasers,
-		'news_stories': NewsStory.objects.order_by('-created_at')[:6],
-		'forum_topics': Topic.objects.order_by('-last_post_at').select_related('created_by_user', 'last_post_by_user')[:5],
+		'banner': None,
+		'teasers': Teaser.objects.none(),
+		'news_stories': NewsStory.objects.none(),
+		'forum_topics': Topic.objects.none(),
 	})
