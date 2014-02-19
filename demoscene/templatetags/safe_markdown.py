bd56a748b9e7008f444efae9e505146e788ee7f4
Matt Westcott <matt@west.co.tt>
2014-02-19 22:17:24 +0000

    Aha! Apparently I *can* have a persistent Markdown interpreter, I just have to call reset() on it

--- demoscene/templatetags/safe_markdown.py
+++ demoscene/templatetags/safe_markdown.py
@@ -1,19 +1,17 @@
 from django import template
 from django.utils.safestring import mark_safe
-from django.utils.encoding import smart_str, force_unicode
 import markdown
 from scrubber import Scrubber
 
 register = template.Library()
 
 scrubber = Scrubber(autolink=False, nofollow=False) # Scrubber's autolink doesn't handle ftp://
-
+md = markdown.Markdown(extensions=['nl2br', 'autolink'])
 
 @register.filter(is_safe=True)
 def safe_markdown(value, arg=''):
-	md = markdown.Markdown(extensions=['nl2br', 'autolink'])
 	return mark_safe(
 		scrubber.scrub(
-			md.convert(value)
+			md.reset().convert(value)
 		)
 	)
