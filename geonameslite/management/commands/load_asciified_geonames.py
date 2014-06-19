a7fdca89d3d288ae9a4c68b850832857d2e03274
Matt Westcott <matt@west.co.tt>
2014-06-19 22:39:07 +0100

    Refactor load_asciified_geonames to not eat up all the memory and kill the server

--- geonameslite/management/commands/load_asciified_geonames.py
+++ geonameslite/management/commands/load_asciified_geonames.py
@@ -1,4 +1,5 @@
 from django.core.management.base import NoArgsCommand
+from django.db.models import Max
 
 from geonameslite.models import Locality, AlternateName
 from unidecode import unidecode
@@ -7,75 +8,81 @@ class Command(NoArgsCommand):
 	def clear_tables(self):
 		AlternateName.objects.filter(is_asciified=True).delete()
 
-	names = {}
-	def load_initial_names(self):
-		batch = 100000
-		print "Loading existing locations"
-		loaded = 0
-		for loc in Locality.objects.all():
-			self.names[loc.geonameid] = set([loc.name])
-			loaded += 1
-			if loaded % batch == 0:
-				print "{0:8d} locations loaded".format(loaded)
-
-		print "Loading existing alt names"
-		loaded = 0
-		for alt_name in AlternateName.objects.all():
-			self.names[alt_name.locality_id].add(alt_name.name)
-			loaded += 1
-			if loaded % batch == 0:
-				print "{0:8d} alt names loaded".format(loaded)
-
 	def load_localities(self):
 		print "ASCIIfying locality names"
-		objects = []
-		batch = 10000
+		batch_size = 10000
 		processed = 0
-		for locality in Locality.objects.all():
-			name = unidecode(locality.name)
-			if name != locality.name and name not in self.names[locality.geonameid]:
-				objects.append(AlternateName(
-					locality_id=locality.geonameid,
-					name=name,
-					is_asciified=True
-				))
-				self.names[locality.geonameid].add(name)
-				processed += 1
 
-				if processed % batch == 0:
-					AlternateName.objects.bulk_create(objects)
-					print "{0:8d} locality names ASCIIfied".format(processed)
-					objects = []
+		max_id = Locality.objects.aggregate(Max('geonameid'))['geonameid__max']
+		batch_start = 0
+
+		while batch_start <= max_id:
+			names = {}
+			objects = []
+
+			for locality in Locality.objects.filter(geonameid__gte=batch_start, geonameid__lt=batch_start + batch_size):
+				names.setdefault(locality.geonameid, set())
+				name = unidecode(locality.name)
+				if (
+					name != locality.name
+					and name not in names[locality.geonameid]
+					and not Locality.objects.filter(geonameid=locality.geonameid, name=name).exists()
+					and not AlternateName.objects.filter(locality_id=locality.geonameid, name=name).exists()
+					):
+
+					objects.append(AlternateName(
+						locality_id=locality.geonameid,
+						name=name,
+						is_asciified=True
+					))
+					names[locality.geonameid].add(name)
+					processed += 1
+
+			AlternateName.objects.bulk_create(objects)
+			print "{0:8d} locality names ASCIIfied".format(processed)
+
+			batch_start += batch_size
 
 		AlternateName.objects.bulk_create(objects)
 		print "{0:8d} locality names ASCIIfied".format(processed)
 
 	def load_altnames(self):
 		print "ASCIIfying alt names"
-		objects = []
-		batch = 10000
+		batch_size = 10000
 		processed = 0
-		for alt_name in AlternateName.objects.all():
-			name = unidecode(alt_name.name)
-			if name != alt_name.name and name not in self.names[alt_name.locality_id]:
-				objects.append(AlternateName(
-					locality_id=alt_name.locality_id,
-					name=name,
-					is_asciified=True
-				))
-				self.names[alt_name.locality_id].add(name)
-				processed += 1
 
-				if processed % batch == 0:
-					AlternateName.objects.bulk_create(objects)
-					print "{0:8d} alt names ASCIIfied".format(processed)
-					objects = []
+		max_id = AlternateName.objects.aggregate(Max('id'))['id__max']
+		batch_start = 0
+
+		while batch_start <= max_id:
+			names = {}
+			objects = []
+
+			for alt_name in AlternateName.objects.filter(id__gte=batch_start, id__lt=batch_start + batch_size):
+				names.setdefault(alt_name.locality_id, set())
+				name = unidecode(alt_name.name)
+				if (
+					name != alt_name.name
+					and name not in names[alt_name.locality_id]
+					and not Locality.objects.filter(geonameid=alt_name.locality_id, name=name).exists()
+					and not AlternateName.objects.filter(locality_id=alt_name.locality_id, name=name).exists()
+					):
+					objects.append(AlternateName(
+						locality_id=alt_name.locality_id,
+						name=name,
+						is_asciified=True
+					))
+					names[alt_name.locality_id].add(name)
+					processed += 1
+
+			AlternateName.objects.bulk_create(objects)
+			print "{0:8d} alt names ASCIIfied".format(processed)
+			batch_start += batch_size
 
 		AlternateName.objects.bulk_create(objects)
 		print "{0:8d} alt names ASCIIfied".format(processed)
 
 	def handle_noargs(self, **options):
 		self.clear_tables()
-		self.load_initial_names()
 		self.load_localities()
 		self.load_altnames()
