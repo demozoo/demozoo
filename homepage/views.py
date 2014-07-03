1000a675052c81d4c646e0caa1b5cacee6e81dc6
Matt Westcott <matt@west.co.tt>
2014-07-03 22:01:31 +0100

    Latest releases panel on homepage

--- homepage/views.py
+++ homepage/views.py
@@ -2,20 +2,25 @@ from django.shortcuts import render
 
 from homepage.models import Banner, NewsStory
 from forums.models import Topic
+from productions.models import Production
 
 def home(request):
 	if request.user.is_authenticated():
-		banners = Banner.objects.filter(show_for_logged_in_users=True).order_by('-created_at')[:1]
+		banner = Banner.objects.filter(show_for_logged_in_users=True).order_by('-created_at').first()
 	else:
-		banners = Banner.objects.filter(show_for_anonymous_users=True).order_by('-created_at')[:1]
+		banner = Banner.objects.filter(show_for_anonymous_users=True).order_by('-created_at').first()
 
-	try:
-		banner = banners[0]
-	except IndexError:
-		banner = None
+	latest_releases = Production.objects.filter(
+		default_screenshot__isnull=False, release_date_date__isnull=False
+	).select_related(
+		'default_screenshot'
+	).prefetch_related(
+		'author_nicks', 'author_affiliation_nicks', 'platforms', 'types'
+	).order_by('-release_date_date', '-created_at')[:5]
 
 	return render(request, 'homepage/home.html', {
 		'banner': banner,
 		'news_stories': NewsStory.objects.order_by('-created_at')[:6],
 		'forum_topics': Topic.objects.order_by('-last_post_at').select_related('created_by_user', 'last_post_by_user')[:5],
+		'latest_releases': latest_releases,
 	})
