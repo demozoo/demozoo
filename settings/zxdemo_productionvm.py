eab10954ff42c74abea345f94b37a9694b278c66
Matt Westcott <matt@west.co.tt>
2014-07-14 23:00:45 +0100

    add productionvm config for zxdemo

---
+++ settings/zxdemo_productionvm.py
@@ -0,0 +1,14 @@
+from .productionvm import *
+
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
+TEMPLATE_CONTEXT_PROCESSORS += (
+	'django.core.context_processors.request',
+	'zxdemo.context_processors.zxdemo_context',
+)
+
+ROOT_URLCONF = 'zxdemo.urls'
+
+ZXDEMO_PLATFORM_IDS = [2]
+
+GEVENT_ADDR_PORT = "localhost:4802"
+ALLOWED_HOSTS = ['zxdemo.org', 'www2.zxdemo.org', 'localhost']
