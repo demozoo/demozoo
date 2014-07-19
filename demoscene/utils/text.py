c1ac562725d513a778187054ccd2bf82ba84df14
Matt Westcott <matt@west.co.tt>
2014-07-19 16:06:43 +0100

    add a generate_sort_key function to convert titles into strings suitable for use as a sorting key

--- demoscene/utils/text.py
+++ demoscene/utils/text.py
@@ -10,3 +10,19 @@ def slugify_tag(value):
 	value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
 	value = re.sub('[^\w\s\.-]', '', value).strip().lower()
 	return mark_safe(re.sub('[-\s]+', '-', value))
+
+
+def generate_sort_key(s):
+	# strip accents and punctuation
+	s = ''.join(c for c in unicodedata.normalize('NFD', s)
+		if not unicodedata.combining(c) and unicodedata.category(c)[0] != 'P')
+
+	s = s.strip().lower()
+
+	# pad numbers with leading zeros
+	s = re.sub(r'\d+', lambda m: '%09d' % (int(m.group(0))), s)
+
+	# move "the" / "a" / "an" to the end
+	s = re.sub(r'^(the|a|an)\s+(.*)$', r'\2 \1', s)
+
+	return s
