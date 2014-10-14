e893b16d9427c4ea8b9897993994898d5e878c1b
Matt Westcott <matt@west.co.tt>
2014-10-14 12:01:35 +0100

    add default logger to django.request? lol i have no idea what i'm doing

--- settings/productionvm.py
+++ settings/productionvm.py
@@ -28,38 +28,38 @@ COMPRESS_OFFLINE_CONTEXT = {
 }
 
 LOGGING = {
-   'version': 1,
-   'disable_existing_loggers': True,
-   'formatters': {
-       'standard': {
-           'format': '%(asctime)s [%(levelname)s] %(pathname)s: %(message)s'
-       },
-   },
-   'handlers': {
-       'default': {
-           'level': 'DEBUG',
-           'class': 'logging.handlers.RotatingFileHandler',
-           'filename': '/home/demozoo/log/app.log',
-           'maxBytes': 10485760,
-           'backupCount': 500,
-           'formatter': 'standard',
-       },
-       'mail_admins': {
-           'level': 'ERROR',
-           'filters': [],
-           'class': 'django.utils.log.AdminEmailHandler',
-       }
-   },
-   'loggers': {
-       '': {
-           'handlers': ['default'],
-           'level': 'DEBUG',
-       },
-       'django.request': {
-           'handlers': ['mail_admins'],
-           'level': 'ERROR',
-       }
-   }
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(pathname)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '/home/demozoo/log/app.log',
+            'maxBytes': 10485760,
+            'backupCount': 500,
+            'formatter': 'standard',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': [],
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+        },
+        'django.request': {
+            'handlers': ['default', 'mail_admins'],
+            'level': 'DEBUG',
+        }
+    }
 }
 
 try:
