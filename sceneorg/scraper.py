2b9535e8820edd3686400cf19b542766bd4648f0
Matt Westcott <matt@west.co.tt>
2015-03-05 00:27:48 +0000

    Update sceneorg.scraper logic for new scene.org frontend (but remove the 'new files' stuff, because that doesn't exist any more...)

--- sceneorg/scraper.py
+++ sceneorg/scraper.py
@@ -7,31 +7,19 @@ user_agent = 'Demozoo/2.0 (gasman@raww.org; http://demozoo.org/)'
 def scrape_dir(url):
 	req = urllib2.Request(url, None, {'User-Agent': user_agent})
 	page = urllib2.urlopen(req)
-	soup = BeautifulSoup(page, fromEncoding="ISO-8859-1")
+	soup = BeautifulSoup(page)
 
 	files = []
-	for entry in soup.findAll('td', 'filelistentry'):
-		if entry.find('img')['src'] == 'img/icons/parent.gif':
+	for entry in soup.findAll('li', 'file'):
+		try:
+			classnames = entry['class'].split(' ')
+		except KeyError:
+			classnames = []
+
+		if 'parent' in classnames:
 			continue
-		is_dir = (entry.find('img')['src'] == 'img/icons/dir.gif')
-		filename = entry.find('td', 'browseleft').text
-
-		files.append((filename, is_dir, None))
-
-	return files
-
-
-def scrape_new_files_dir(url):
-	req = urllib2.Request(url, None, {'User-Agent': user_agent})
-	page = urllib2.urlopen(req)
-	soup = BeautifulSoup(page, fromEncoding="ISO-8859-1")
-
-	files = []
-	for entry in soup.findAll('td', 'newfile'):
-		if entry.find('img')['src'] == 'img/icons/parent.gif':
-			continue
-		is_dir = (entry.find('img')['src'] == 'img/icons/dir.gif')
-		filename = entry.find('td', 'newfilesleft').text
+		is_dir = ('directory' in classnames)
+		filename = entry.find('span', 'filename').text
 
 		files.append((filename, is_dir, None))
 
