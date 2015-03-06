cb86cdd9771c1dd41e92cd8de2aeaf6335b42548
Matt Westcott <matt@west.co.tt>
2015-03-06 22:27:49 +0000

    assorted API additions

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from demoscene.models import Releaser, Nick, Membership, ReleaserExternalLink
-from productions.models import Production, ProductionLink
+from productions.models import Production, ProductionLink, Credit
 
 
 class NickSerializer(serializers.ModelSerializer):
@@ -9,11 +9,6 @@ class NickSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Nick
 		fields = ['name', 'abbreviation', 'is_primary_nick', 'variants']
-
-class NickSummarySerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Nick
-		fields = ['name', 'abbreviation']
 
 class ReleaserSummarySerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
@@ -31,6 +26,16 @@ class MemberMembershipSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Membership
 		fields = ['member', 'is_current']
+
+class SubgroupMembershipSerializer(serializers.ModelSerializer):
+	subgroup = serializers.SerializerMethodField()
+
+	def get_subgroup(self, membership):
+		return ReleaserSummarySerializer(instance=membership.member, context=self.context).data
+
+	class Meta:
+		model = Membership
+		fields = ['subgroup', 'is_current']
 
 class ReleaserExternalLinkSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -54,23 +59,42 @@ class ReleaserSerializer(serializers.HyperlinkedModelSerializer):
 
 	def get_subgroups(self, releaser):
 		member_memberships = releaser.member_memberships.filter(member__is_group=True).select_related('member')
-		return MemberMembershipSerializer(instance=member_memberships, many=True, context=self.context).data
+		return SubgroupMembershipSerializer(instance=member_memberships, many=True, context=self.context).data
 
 	class Meta:
 		model = Releaser
 		fields = ['url', 'id', 'name', 'is_group', 'nicks', 'member_of', 'members', 'subgroups', 'external_links']
 
 
+class AuthorSerializer(serializers.HyperlinkedModelSerializer):
+	class Meta:
+		model = Releaser
+		fields = ['url', 'id', 'name', 'is_group']
+
+class AuthorNickSerializer(serializers.ModelSerializer):
+	releaser = AuthorSerializer(read_only=True)
+	class Meta:
+		model = Nick
+		fields = ['name', 'abbreviation', 'releaser']
+
 class ProductionExternalLinkSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = ProductionLink
 		fields = ['link_class', 'url']
 
+class ProductionCreditSerializer(serializers.ModelSerializer):
+	nick = AuthorNickSerializer(read_only=True)
+	class Meta:
+		model = Credit
+		fields = ['nick', 'category', 'role']
+
 class ProductionSerializer(serializers.HyperlinkedModelSerializer):
-	author_nicks = NickSummarySerializer(many=True, read_only=True)
+	author_nicks = AuthorNickSerializer(many=True, read_only=True)
+	author_affiliation_nicks = AuthorNickSerializer(many=True, read_only=True)
+	credits = ProductionCreditSerializer(many=True, read_only=True)
 	download_links = ProductionExternalLinkSerializer(many=True, read_only=True)
 	external_links = ProductionExternalLinkSerializer(many=True, read_only=True)
 
 	class Meta:
 		model = Production
-		fields = ['url', 'id', 'title', 'author_nicks', 'download_links', 'external_links']
+		fields = ['url', 'id', 'title', 'author_nicks', 'author_affiliation_nicks', 'credits', 'download_links', 'external_links']
