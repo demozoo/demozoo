a6f57d76958120280f93c7f94f51771fe26c6f09
Matt Westcott <matt@west.co.tt>
2015-06-06 01:24:06 +0100

    update Eq's custom API to determine nationalities from group members if it isn't clear from authors/credits alone

--- api/views/adhoc/eq.py
+++ api/views/adhoc/eq.py
@@ -2,6 +2,7 @@ import csv
 
 from django.http import HttpResponse
 
+from demoscene.models import Releaser
 from productions.models import Production
 from platforms.models import Platform
 
@@ -28,8 +29,18 @@ def demos(request):
 	])
 	for prod in prods:
 		prod_types = sorted(prod.types.all(), key=lambda t: t.name)
-		nationalities = set([nick.releaser.country_code for nick in prod.author_nicks.all()]).union([credit.nick.releaser.country_code for credit in prod.credits.all()])
+		# try to assemble a list of nationalities based on the sceners listed under authors or credits
+		authors = [nick.releaser for nick in prod.author_nicks.all()]
+		credited_sceners = [credit.nick.releaser for credit in prod.credits.all()]
+		nationalities = set([author.country_code for author in authors]).union([scener.country_code for scener in credited_sceners])
 		nationalities = list(n for n in nationalities if n)
+		# if this results in an empty list, see if it's entirely written by groups whose members all belong to one country
+		if (not nationalities) and all(author.is_group for author in authors):
+			members = Releaser.objects.filter(group_memberships__group__in=authors)
+			nationalities = list(set([member.country_code for member in members]))
+			if len(nationalities) != 1 or nationalities[0] == '':
+				nationalities = []
+
 		csvfile.writerow([
 			prod.id,
 			(u'http://demozoo.org' + prod.get_absolute_url()).encode('utf-8'),
