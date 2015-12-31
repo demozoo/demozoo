898f2d90a298ef590c6cfc3cc8e3e87a38ae1f21
Matt Westcott <matt@west.co.tt>
2015-12-31 00:02:28 +0000

    Use single screenshot as video thumbnail where possible

--- productions/carousel.py
+++ productions/carousel.py
@@ -13,26 +13,6 @@ def get_mosaic_data(processed_screenshots):
 	]
 
 
-def get_video_data(video):
-	if video.thumbnail_width > 400 or video.thumbnail_height > 300:
-		scale_factor = min(400.0 / video.thumbnail_width, 300.0 / video.thumbnail_height)
-		width = round(video.thumbnail_width * scale_factor)
-		height = round(video.thumbnail_height * scale_factor)
-	else:
-		width = video.thumbnail_width
-		height = video.thumbnail_height
-
-	return {
-		'url': str(video.link),
-		'video_width': video.video_width,
-		'video_height': video.video_height,
-		'embed_code': video.link.get_embed_html(video.video_width, video.video_height),
-		'thumbnail_url': video.thumbnail_url,
-		'thumbnail_width': width,
-		'thumbnail_height': height
-	}
-
-
 def get_carousel_items(production):
 	screenshots = production.screenshots.order_by('id')
 	processed_screenshots = [s for s in screenshots if s.original_url]
@@ -56,9 +36,35 @@ def get_carousel_items(production):
 
 	if embeddable_videos:
 		video = embeddable_videos[0]
-		video_data = get_video_data(video)
+		video_data = {
+			'url': str(video.link),
+			'video_width': video.video_width,
+			'video_height': video.video_height,
+			'embed_code': video.link.get_embed_html(video.video_width, video.video_height),
+		}
+
 		if len(processed_screenshots) >= 4:
 			video_data['mosaic'] = get_mosaic_data(processed_screenshots)
+		else:
+			if len(processed_screenshots) >= 1:
+				screenshot = random.choice(processed_screenshots)
+				thumbnail_url = screenshot.standard_url
+				thumbnail_width = screenshot.standard_width
+				thumbnail_height = screenshot.standard_height
+			else:
+				thumbnail_url = video.thumbnail_url
+				thumbnail_width = video.thumbnail_width
+				thumbnail_height = video.thumbnail_height
+
+			if thumbnail_width > 400 or thumbnail_height > 300:
+				scale_factor = min(400.0 / thumbnail_width, 300.0 / thumbnail_height)
+				video_data['thumbnail_width'] = round(thumbnail_width * scale_factor)
+				video_data['thumbnail_height'] = round(thumbnail_height * scale_factor)
+			else:
+				video_data['thumbnail_width'] = thumbnail_width
+				video_data['thumbnail_height'] = thumbnail_height
+
+			video_data['thumbnail_url'] = thumbnail_url
 
 		carousel_data.insert(0, {
 			'type': 'video',
