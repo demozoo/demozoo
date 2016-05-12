be2569c64fe5f1b4537b362ededa844b00db1af6
Matt Westcott <matt@west.co.tt>
2016-05-12 22:15:04 +0100

    Reorganise carousel JS and gracefully handle unrecognised carousel item types

--- productions/static/productions/js/components/carousel.js
+++ productions/static/productions/js/components/carousel.js
@@ -1,148 +1,153 @@
 (function($) {
-	$.fn.carousel = function(carouselData, reloadUrl) {
-		if (carouselData.length === 0) return;
 
-		function Screenshot(fullData) {
-			this.isProcessing = fullData['is_processing'];
-			this.id = fullData['id'];
-			this.data = fullData.data;
+	/* Define carousel item types */
+
+	function Screenshot(fullData) {
+		this.isProcessing = fullData['is_processing'];
+		this.id = fullData['id'];
+		this.data = fullData.data;
+	}
+	Screenshot.prototype.preload = function() {
+		var src = this.data['standard_url'];
+		var img = new Image();
+		img.src = src;
+	};
+	Screenshot.prototype.draw = function(container) {
+		if (this.isProcessing) {
+			container.html('<div class="screenshot"><img src="/static/images/screenshot_loading.gif" width="32" height="32" alt="" /></div>');
+		} else {
+			var link = $('<a class="screenshot"></a>').attr({'href': this.data['original_url']});
+			var img = $('<img>').attr({'src': this.data['standard_url'], 'width': this.data['standard_width'], 'height': this.data['standard_height']});
+			link.append(img);
+			container.html(link);
+			link.openImageInLightbox();
 		}
-		Screenshot.prototype.preload = function() {
-			var src = this.data['standard_url'];
+	};
+
+	function buildMosaic(items, addLinks) {
+		var width = 0, height = 0, i;
+		for (i = 0; i < items.length; i++) {
+			width = Math.max(width, items[i]['standard_width']);
+			height = Math.max(height, items[i]['standard_height']);
+		}
+		var mosaic = $('<div class="mosaic"></div>').css({'width': width + 'px', 'height': height + 'px'});
+		for (i = 0; i < items.length; i++) {
+			var imgData = items[i];
+			var tile;
+			if (addLinks) {
+				tile = $('<a class="tile"></a>').attr({
+					'href': imgData['original_url']
+				});
+				tile.openImageInLightbox();
+			} else {
+				tile = $('<div class="tile"></div>');
+			}
+			tile.css({
+				'width': width/2 + 'px',
+				'height': height/2 + 'px',
+				'line-height': height/2 + 'px'
+			});
+			var img = $('<img>').attr({
+				'src': imgData['standard_url'],
+				'width': imgData['standard_width'] / 2,
+				'height': imgData['standard_height'] / 2
+			});
+			tile.append(img);
+			mosaic.append(tile);
+		}
+		return mosaic;
+	}
+
+	function Mosaic(fullData) {
+		this.isProcessing = fullData['is_processing'];
+		this.id = fullData['id'];
+		this.data = fullData.data;
+	}
+
+	Mosaic.prototype.preload = function() {
+		for (var i = 0; i < this.data.length; i++) {
+			var src = this.data[i]['standard_url'];
 			var img = new Image();
 			img.src = src;
-		};
-		Screenshot.prototype.draw = function(container) {
-			if (this.isProcessing) {
-				container.html('<div class="screenshot"><img src="/static/images/screenshot_loading.gif" width="32" height="32" alt="" /></div>');
-			} else {
-				var link = $('<a class="screenshot"></a>').attr({'href': this.data['original_url']});
-				var img = $('<img>').attr({'src': this.data['standard_url'], 'width': this.data['standard_width'], 'height': this.data['standard_height']});
-				link.append(img);
-				container.html(link);
-				link.openImageInLightbox();
-			}
-		};
-
-		function buildMosaic(items, addLinks) {
-			var width = 0, height = 0, i;
-			for (i = 0; i < items.length; i++) {
-				width = Math.max(width, items[i]['standard_width']);
-				height = Math.max(height, items[i]['standard_height']);
-			}
-			var mosaic = $('<div class="mosaic"></div>').css({'width': width + 'px', 'height': height + 'px'});
-			for (i = 0; i < items.length; i++) {
-				var imgData = items[i];
-				var tile;
-				if (addLinks) {
-					tile = $('<a class="tile"></a>').attr({
-						'href': imgData['original_url']
-					});
-					tile.openImageInLightbox();
-				} else {
-					tile = $('<div class="tile"></div>');
-				}
-				tile.css({
-					'width': width/2 + 'px',
-					'height': height/2 + 'px',
-					'line-height': height/2 + 'px'
-				});
-				var img = $('<img>').attr({
-					'src': imgData['standard_url'],
-					'width': imgData['standard_width'] / 2,
-					'height': imgData['standard_height'] / 2
-				});
-				tile.append(img);
-				mosaic.append(tile);
-			}
-			return mosaic;
 		}
+	};
+	Mosaic.prototype.draw = function(container) {
+		/* use the largest screenshot dimension as the mosaic size;
+		each tile will be half this in each direction, padded as necessary.
+		If all screenshots are equal size (hopefully the most common case),
+		no padding will be needed. */
+		var mosaic = buildMosaic(this.data, true);
+		container.html(mosaic);
+	};
 
-		function Mosaic(fullData) {
-			this.isProcessing = fullData['is_processing'];
-			this.id = fullData['id'];
-			this.data = fullData.data;
+	function Video(fullData) {
+		this.isProcessing = fullData['is_processing'];
+		this.id = fullData['id'];
+		this.data = fullData.data;
+	}
+	Video.prototype.preload = function() {
+		for (var i = 0; i < this.data.length; i++) {
+			var src = this.data['thumbnail_url'];
+			var img = new Image();
+			img.src = src;
 		}
+	};
+	Video.prototype.draw = function(container) {
+		var videoData = this.data;
 
-		Mosaic.prototype.preload = function() {
-			for (var i = 0; i < this.data.length; i++) {
-				var src = this.data[i]['standard_url'];
-				var img = new Image();
-				img.src = src;
-			}
-		};
-		Mosaic.prototype.draw = function(container) {
-			/* use the largest screenshot dimension as the mosaic size;
-			each tile will be half this in each direction, padded as necessary.
-			If all screenshots are equal size (hopefully the most common case),
-			no padding will be needed. */
-			var mosaic = buildMosaic(this.data, true);
-			container.html(mosaic);
-		};
-
-		function Video(fullData) {
-			this.isProcessing = fullData['is_processing'];
-			this.id = fullData['id'];
-			this.data = fullData.data;
-		}
-		Video.prototype.preload = function() {
-			for (var i = 0; i < this.data.length; i++) {
-				var src = this.data['thumbnail_url'];
-				var img = new Image();
-				img.src = src;
-			}
-		};
-		Video.prototype.draw = function(container) {
-			var videoData = this.data;
-
-			var link = $('<a class="video"><div class="play"></div></a>').attr({'href': this.data['url']});
-			var img;
-			if (videoData['mosaic']) {
-				img = buildMosaic(videoData['mosaic'], false);
-			} else {
-				img = $('<img>').attr({
-					'src': this.data['thumbnail_url'],
-					'width': this.data['thumbnail_width'],
-					'height': this.data['thumbnail_height']
-				});
-			}
-			link.prepend(img);
-			container.html(link);
-
-			link.click(function() {
-				var videoElement = $(videoData['embed_code']);
-				var lightbox = MediaLightbox(function(maxWidth, maxHeight) {
-					var videoWidth = videoData['video_width'];
-					var videoHeight = videoData['video_height'];
-
-					var fullWidth = Math.min(videoWidth, maxWidth);
-					var fullHeight = Math.min(videoHeight, maxHeight);
-
-					var heightAtFullWidth = (fullWidth * videoHeight/videoWidth);
-					var widthAtFullHeight = (fullHeight * videoWidth/videoHeight);
-
-					if (heightAtFullWidth <= maxHeight) {
-						finalWidth = fullWidth;
-						finalHeight = Math.round(heightAtFullWidth);
-					} else {
-						finalWidth = Math.round(widthAtFullHeight);
-						finalHeight = fullHeight;
-					}
-
-					videoElement.attr('width', finalWidth);
-					videoElement.attr('height', finalHeight);
-					return [finalWidth, finalHeight];
-				});
-				lightbox.mediaWrapper.append(videoElement);
-				return false;
+		var link = $('<a class="video"><div class="play"></div></a>').attr({'href': this.data['url']});
+		var img;
+		if (videoData['mosaic']) {
+			img = buildMosaic(videoData['mosaic'], false);
+		} else {
+			img = $('<img>').attr({
+				'src': this.data['thumbnail_url'],
+				'width': this.data['thumbnail_width'],
+				'height': this.data['thumbnail_height']
 			});
-		};
+		}
+		link.prepend(img);
+		container.html(link);
 
-		var itemTypes = {
-			'screenshot': Screenshot,
-			'mosaic': Mosaic,
-			'video': Video
-		};
+		link.click(function() {
+			var videoElement = $(videoData['embed_code']);
+			var lightbox = MediaLightbox(function(maxWidth, maxHeight) {
+				var videoWidth = videoData['video_width'];
+				var videoHeight = videoData['video_height'];
+
+				var fullWidth = Math.min(videoWidth, maxWidth);
+				var fullHeight = Math.min(videoHeight, maxHeight);
+
+				var heightAtFullWidth = (fullWidth * videoHeight/videoWidth);
+				var widthAtFullHeight = (fullHeight * videoWidth/videoHeight);
+
+				if (heightAtFullWidth <= maxHeight) {
+					finalWidth = fullWidth;
+					finalHeight = Math.round(heightAtFullWidth);
+				} else {
+					finalWidth = Math.round(widthAtFullHeight);
+					finalHeight = fullHeight;
+				}
+
+				videoElement.attr('width', finalWidth);
+				videoElement.attr('height', finalHeight);
+				return [finalWidth, finalHeight];
+			});
+			lightbox.mediaWrapper.append(videoElement);
+			return false;
+		});
+	};
+
+	var itemTypes = {
+		'screenshot': Screenshot,
+		'mosaic': Mosaic,
+		'video': Video
+	};
+
+	/* Constructor for a carousel */
+
+	$.fn.carousel = function(carouselData, reloadUrl) {
+		if (carouselData.length === 0) return;
 
 		var carouselItems = [];
 		var carouselItemsById = {};
@@ -150,10 +155,17 @@
 		var currentIndex = 0;
 
 		var viewport = this;
-		viewport.html('<div class="viewport"><div class="tray"></div></div>');
-		var tray = $('.tray', viewport);
-		var currentBucket = $('<div class="bucket"></div>');
-		tray.append(currentBucket);
+		var tray;
+		var currentBucket;
+
+		var hasInitialisedViewport = false;
+		function initViewport() {
+			viewport.html('<div class="viewport"><div class="tray"></div></div>');
+			tray = $('.tray', viewport);
+			currentBucket = $('<div class="bucket"></div>');
+			tray.append(currentBucket);
+			hasInitialisedViewport = true;
+		}
 
 		var hasPreloadedAllImages = false;
 
@@ -164,28 +176,42 @@
 
 			var foundCurrentId = false;
 			var needReload = false;
+			var itemCount = 0;
 
 			for (var i = 0; i < carouselData.length; i++) {
+				/* look for an existing carousel item with this ID */
 				var carouselItem = carouselItemsById[carouselData[i].id];
+
 				if (!carouselItem || carouselItem.isProcessing) {
+					/* item not already found, or was previously in processing, so create it as new */
 					itemType = itemTypes[carouselData[i].type];
-					carouselItem = new itemType(carouselData[i]);
+					if (itemType) {
+						carouselItem = new itemType(carouselData[i]);
+					} else {
+						/* skip unidentified item types */
+						continue;
+					}
 				}
-				carouselItems[i] = carouselItem;
+				carouselItems[itemCount] = carouselItem;
 				newCarouselItemsById[carouselData[i].id] = carouselItem;
 
 				if (currentId == carouselData[i].id) {
 					foundCurrentId = true;
-					currentIndex = i;
+					currentIndex = itemCount;
 				}
 				if (carouselData[i]['is_processing']) {
 					needReload = true;
 				}
+				itemCount++;
 			}
+
+			if (itemCount === 0) return;
+
+			if (!hasInitialisedViewport) initViewport();
 
 			if (!foundCurrentId) {
 				currentIndex = 0;
-				currentId = carouselData[0].id;
+				currentId = carouselItems[0].id;
 			}
 
 			carouselItems[currentIndex].draw(currentBucket);
