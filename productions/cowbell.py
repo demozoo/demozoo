43d192c5812ebc560086e553e3109393567f0aae
Matt Westcott <matt@west.co.tt>
2016-05-14 01:02:59 +0100

    Add scene.org mp3/ogg links (which don't need CORS) to recognised Cowbell-compatible tracks

--- productions/cowbell.py
+++ productions/cowbell.py
@@ -30,9 +30,12 @@ PLAYERS_BY_FILETYPE = {
 			'productions/js/cowbell/vtx.min.js'
 		])
 	),
+	'mp3': ('Cowbell.Player.Audio', {}, Media(js=['productions/js/cowbell/cowbell.min.js'])),
+	'ogg': ('Cowbell.Player.Audio', {}, Media(js=['productions/js/cowbell/cowbell.min.js'])),
 }
 
 ZXDEMO_MUSIC = re.compile(r'https?://files\.zxdemo\.org/.*\.(stc|pt3|vtx)$', re.I)
+STREAMING_MUSIC = re.compile(r'.*\.(mp3|ogg)$', re.I)
 
 
 def get_playable_track_data(production):
@@ -40,19 +43,27 @@ def get_playable_track_data(production):
 	tracks = []
 	combined_media = Media()
 	for link in production.download_links:
-		if link.link_class == 'BaseUrl':
+		match = None
+
+		if link.link_class == 'SceneOrgFile':
+			match = STREAMING_MUSIC.match(link.parameter)
+			if match:
+				filetype = match.group(1).lower()
+				url = link.link.nl_http_url
+		elif link.link_class == 'BaseUrl':
 			url = link.parameter
 			match = ZXDEMO_MUSIC.match(url)
 			if match:
 				filetype = match.group(1).lower()
-				player, player_opts, media = PLAYERS_BY_FILETYPE[filetype]
-				combined_media += media
-				tracks.append({
-					'id': link.id,
-					'url': url,
-					'player': player,
-					'playerOpts': player_opts
-				})
-				continue
+
+		if match:
+			player, player_opts, media = PLAYERS_BY_FILETYPE[filetype]
+			combined_media += media
+			tracks.append({
+				'id': link.id,
+				'url': url,
+				'player': player,
+				'playerOpts': player_opts
+			})
 
 	return tracks, combined_media
