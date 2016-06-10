ced781c95cea1aedce5ba148657f3c37e081eacd
Matt Westcott <matt@west.co.tt>
2016-06-10 22:09:10 +0100

    switch cowbell links hosted on files.zxdemo.org and modarchive.org to HTTPS

--- productions/cowbell.py
+++ productions/cowbell.py
@@ -40,7 +40,7 @@ PLAYERS_BY_FILETYPE = {
 	])),
 }
 
-ZXDEMO_MUSIC = re.compile(r'https?://files\.zxdemo\.org/.*\.(stc|pt3|vtx)$', re.I)
+ZXDEMO_MUSIC = re.compile(r'https://files\.zxdemo\.org/.*\.(stc|pt3|vtx)$', re.I)
 ZX_MUSIC = re.compile(r'.*\.(stc|pt3|vtx)$', re.I)
 STREAMING_MUSIC = re.compile(r'.*\.(mp3|ogg)$', re.I)
 OPENMPT_MUSIC = re.compile(r'.*\.(mod|s3m|xm|it|mptm|stm|nst|m15|stk|wow|ult|669|mtm|med|far|mdl|ams|dsm|amf|okt|dmf|ptm|psm|mt2|dbm|digi|imf|j2b|gdm|umx|plm|mo3|xpk|ppm|mmcmp|sfx|sfx2|mms|pt36)$', re.I)
@@ -80,7 +80,7 @@ def identify_link_as_track(link):
 	else:  # External link
 		if link.link_class == 'ModarchiveModule':
 			filetype = 'openmpt'
-			url = 'http://modarchive.org/jsplayer.php?moduleid=%s' % link.parameter
+			url = 'https://modarchive.org/jsplayer.php?moduleid=%s' % link.parameter
 			return (filetype, url)
 
 	# no match
