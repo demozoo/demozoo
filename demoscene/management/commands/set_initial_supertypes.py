1d01fe53ec5ee471a48ff296116cd9395fa29145
Matt Westcott <matt@west.co.tt>
2016-09-21 21:45:30 +0100

    Remove obsolete management commands

--- demoscene/management/commands/set_initial_supertypes.py
+++
@@ -1,7 +0,0 @@
-from productions.models import Production
-from django.core.management.base import NoArgsCommand
-
-class Command(NoArgsCommand):
-	def handle_noargs(self, **options):
-		for prod in Production.objects.all():
-			prod.save() # save() trigger will sync with inferred supertype. Gosh, that was easy.
