d4f85df36c79b117bfe4d1372c864da693bc922f
Matt Westcott <matt@west.co.tt>
2017-04-24 23:18:26 +0100

    Invoke WSGI with the correct incantation (rather than one that was probably last current in Django 1.3)

--- wsgi_staging.py
+++ wsgi_staging.py
@@ -1,4 +1,5 @@
 import os
-import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'demozoo.settings.staging'
-application = django.core.handlers.wsgi.WSGIHandler()
+application = get_wsgi_application()
