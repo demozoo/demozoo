2f3ec3c6c1c9d44aeabb4e19388d6c95b188dcb0
Matt Westcott <matt@west.co.tt>
2017-04-24 20:30:42 +0100

    Point wsgi_staging.py to correct settings

--- wsgi_staging.py
+++ wsgi_staging.py
@@ -1,4 +1,4 @@
 import os
 import django.core.handlers.wsgi
-os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.staging'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'demozoo.settings.staging'
 application = django.core.handlers.wsgi.WSGIHandler()
