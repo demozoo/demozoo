7ccec7332632bad25401f9e77fbc423495d0bcd3
Matt Westcott <matt@west.co.tt>
2017-07-24 21:58:56 +0100

    Make .psid files on Modland playable in Cowbell

--- productions/cowbell.py
+++ productions/cowbell.py
@@ -46,12 +46,20 @@ PLAYERS_BY_FILETYPE = {
 	}, Media(js=[
 		'productions/js/cowbell/cowbell.min.js', 'productions/js/cowbell/openmpt.min.js'
 	])),
+	'sid': (
+		'Cowbell.Player.JSSID', {},
+		Media(js=[
+			'productions/js/cowbell/cowbell.min.js', 'productions/js/cowbell/jssid.min.js'
+		])
+	),
 }
 
 ZXDEMO_MUSIC = re.compile(r'https://files\.zxdemo\.org/.*\.(stc|pt3|vtx|sqt)$', re.I)
 ZX_MUSIC = re.compile(r'.*\.(stc|pt3|vtx|sqt)$', re.I)
 STREAMING_MUSIC = re.compile(r'.*\.(mp3|ogg)$', re.I)
 OPENMPT_MUSIC = re.compile(r'.*\.(mod|s3m|xm|it|mptm|stm|nst|m15|stk|wow|ult|669|mtm|med|far|mdl|ams|dsm|amf|okt|dmf|ptm|psm|mt2|dbm|digi|imf|j2b|gdm|umx|plm|mo3|xpk|ppm|mmcmp|sfx|sfx2|mms|pt36|nt|ft)$', re.I)
+# SID files on Modland have the extension .psid; .sid files on there are actually Amiga Sidmon tracker files
+PSID_MUSIC = re.compile(r'.*\.psid$', re.I)
 
 
 def identify_link_as_track(link):
@@ -75,6 +83,12 @@ def identify_link_as_track(link):
 			match = ZX_MUSIC.match(link.parameter)
 			if match:
 				filetype = match.group(1).lower()
+				url = 'https://ftp.modland.com%s' % link.parameter
+				return (filetype, url)
+
+			match = PSID_MUSIC.match(link.parameter)
+			if match:
+				filetype = 'sid'
 				url = 'https://ftp.modland.com%s' % link.parameter
 				return (filetype, url)
 
