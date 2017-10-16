bcc2970ef990e2dc7210e70357ea35b158fc59b0
Matt Westcott <matt@west.co.tt>
2017-10-16 22:08:38 +0100

    Fix context processor settings for zxdemo.org

--- demozoo/settings/zxdemo_staging.py
+++ demozoo/settings/zxdemo_staging.py
@@ -1,10 +1,17 @@
 from .staging import *
 
-from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS
-TEMPLATE_CONTEXT_PROCESSORS += (
-	'django.core.context_processors.request',
+TEMPLATES['OPTIONS']['context_processors'] = [
+	'django.contrib.auth.context_processors.auth',
+	'django.template.context_processors.debug',
+	'django.template.context_processors.i18n',
+	'django.template.context_processors.media',
+	'django.template.context_processors.static',
+	'django.template.context_processors.tz',
+	'django.contrib.messages.context_processors.messages',
+
+	'django.template.context_processors.request',
 	'zxdemo.context_processors.zxdemo_context',
-)
+]
 
 ROOT_URLCONF = 'zxdemo.urls'
 
