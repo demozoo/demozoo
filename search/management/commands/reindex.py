54d511b74b7d299c6e18e68041ccaa36e8e13930
Matt Westcott <matt@west.co.tt>
2018-02-12 21:45:35 +0000

    Fetch objects for indexing in batches

--- search/management/commands/reindex.py
+++ search/management/commands/reindex.py
@@ -6,10 +6,17 @@ class Command(BaseCommand):
 	help = "Re-indexes all productions"
 
 	def handle(self, *args, **kwargs):
+		BATCH_SIZE = 10000
+
 		for klass in (Production, ):
 			i = 0
-			for obj in klass.objects.prefetch_related('tags').order_by('pk'):
-				obj.save()
-				i += 1
-				if i % 100 == 0:
-					print klass, i
+			while True:
+				batch = klass.objects.prefetch_related('tags').order_by('pk')[i:(i + BATCH_SIZE)]
+				for obj in batch:
+					obj.save()
+					i += 1
+					if i % 100 == 0:
+						print klass, i
+
+				if len(batch) < BATCH_SIZE:
+					break
