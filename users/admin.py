5a15d8ae1adbaa39d8fcfdfa05a0621f2abd2dca
Matt Westcott <matt@west.co.tt>
2018-04-18 01:32:28 +0100

    Remove the delete button from user admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+
+class UndeletableUserAdmin(UserAdmin):
+	# Customise the admin for django.contrib.auth.user to never provide the delete button.
+	# Instead of deleting, users should be disabled by setting the 'active' flag to false.
+	def has_delete_permission(self, request, obj=None):
+		return False
+
+
+admin.site.unregister(User)
+admin.site.register(User, UndeletableUserAdmin)
