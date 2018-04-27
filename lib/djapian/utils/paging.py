5fe796538681f140f210c6977198d56122edd724
Matt Westcott <matt@west.co.tt>
2018-04-27 20:14:30 +0100

    Remove Djapian \o/

--- lib/djapian/utils/paging.py
+++
@@ -1,7 +0,0 @@
-from django.core.paginator import Paginator
-
-def paginate(queue, per_page):
-    paginator = Paginator(queue, per_page)
-
-    for num in paginator.page_range:
-        yield paginator.page(num)
