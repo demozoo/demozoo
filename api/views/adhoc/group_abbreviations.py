02baa6dda5a1eb7ba9361eb98354b2f3ba9a60d3
Matt Westcott <matt@west.co.tt>
2018-09-08 23:48:35 +0100

    revise group-abbreviations API to include other variants and split by nick

--- api/views/adhoc/group_abbreviations.py
+++ api/views/adhoc/group_abbreviations.py
@@ -2,24 +2,25 @@ import csv
 
 from django.http import HttpResponse
 
-from demoscene.models import Releaser
+from demoscene.models import Nick
 
 
 def group_abbreviations(request):
-	groups = Releaser.objects.filter(is_group=True).prefetch_related('nicks').order_by('name')
+	nicks = Nick.objects.filter(releaser__is_group=True).select_related('releaser').prefetch_related('variants').order_by('name')
 
 	response = HttpResponse(content_type='text/plain;charset=utf-8')
 	csvfile = csv.writer(response)
 	csvfile.writerow([
-		'ID', 'Demozoo URL', 'Name', 'Differentiator', 'Abbreviation'
+		'ID', 'Demozoo URL', 'Name', 'Differentiator', 'Abbreviation', 'Other variants'
 	])
-	for group in groups:
+	for nick in nicks:
 		csvfile.writerow([
-			group.id,
-			(u'https://demozoo.org' + group.get_absolute_url()).encode('utf-8'),
-			group.name.encode('utf-8'),
-			group.primary_nick.differentiator.encode('utf-8'),
-			group.abbreviation.encode('utf-8'),
+			nick.releaser.id,
+			(u'https://demozoo.org' + nick.releaser.get_absolute_url()).encode('utf-8'),
+			nick.name.encode('utf-8'),
+			nick.differentiator.encode('utf-8'),
+			nick.abbreviation.encode('utf-8'),
+			nick.nick_variant_list.encode('utf-8')
 		])
 
 	return response
