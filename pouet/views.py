549b544a9382c325e491b8499fd3aa21f6362344
Matt Westcott <matt@west.co.tt>
2018-10-07 19:39:57 +0100

    Add Groups with potential matches | All groups link switches

--- pouet/views.py
+++ pouet/views.py
@@ -11,13 +11,15 @@ from pouet.models import GroupMatchInfo
 def groups(request):
 	# get list of releasers who have Pouet.GroupMatchInfo data
 	groups = GroupMatchInfo.objects.select_related('releaser').order_by('releaser__name').prefetch_related('releaser__nicks')
+	show_full = request.GET.get('full')
 
-	if not request.GET.get('full'):
+	if not show_full:
 		# filter to just the ones which have unmatched entries on both sides
 		groups = groups.filter(unmatched_demozoo_production_count__gt=0, unmatched_pouet_production_count__gt=0)
 
 	return render(request, 'pouet/groups.html', {
 		'groups': groups,
+		'showing_full': show_full,
 	})
 
 
