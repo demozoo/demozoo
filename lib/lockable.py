c27b66dad5faaf43ea8ac7ac82fee283761ccfbc
Matt Westcott <matt@west.co.tt>
2019-01-15 01:38:40 +0000

    Implement view/model-level checks for locked productions

---
+++ lib/lockable.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Lockable(models.Model):
+	locked = models.BooleanField(default=False, editable=False)
+
+	def editable_by_user(self, user):
+		return user.is_authenticated and ((not self.locked) or user.is_staff)
+
+	class Meta:
+		abstract = True
