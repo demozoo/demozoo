ca2ced0384a2bb368123ffcf774023e1619e2459
Matt Westcott <matt@west.co.tt>
2019-06-01 16:11:54 +0200

    Fix Janeway import to only match authors by name if they have >=2 members/groups

--- janeway/management/commands/match_janeway.py
+++ janeway/management/commands/match_janeway.py
@@ -34,10 +34,10 @@ class Command(BaseCommand):
 			if author.is_group:
 				# get this group's member memberships
 				member_ids = Author.objects.filter(group_memberships__group=author).values_list('janeway_id', flat=True)
-				#member_names = [
-				#	generate_search_title(name.name)
-				#	for name in Name.objects.filter(author__group_memberships__group=author)
-				#]
+				member_names = [
+					generate_search_title(name.name)
+					for name in Name.objects.filter(author__group_memberships__group=author)
+				]
 
 				member_demozoo_ids = list(ReleaserExternalLink.objects.filter(
 					link_class='KestraBitworldAuthor', parameter__in=[str(id) for id in member_ids]
@@ -48,26 +48,32 @@ class Command(BaseCommand):
 					member_id__in=member_demozoo_ids
 				).distinct().values_list('group_id', flat=True))
 
-				# see if any candidate releasers have one or more matching members by name
-				# - commented out due to false positives, e.g. JP/Mayhem:
+				# see if any candidate releasers have TWO or more matching members by name
+				# (>=2 avoids false positives such as JP/Mayhem:
 				#   http://janeway.exotica.org.uk/author.php?id=29340
-				#   https://demozoo.org/sceners/40395/
-				#matching_releaser_ids_by_group_name = set(DZMembership.objects.filter(
-				#	group_id__in=candidate_releaser_ids,
-				#	member__nicks__variants__search_title__in=member_names
-				#).distinct().values_list('group_id', flat=True))
+				#   https://demozoo.org/sceners/40395/ )
+				for candidate_releaser_id in candidate_releaser_ids:
+					if candidate_releaser_id in matching_releaser_ids:
+						# ignore ones which are already matched by member ID
+						continue
 
-				#if matching_releaser_ids:
-				#	print("group match for %s: %d => %r" % (author.name, author.janeway_id, matching_releaser_ids))
+					name_match_count = DZMembership.objects.filter(
+						group_id=candidate_releaser_id,
+						member__nicks__variants__search_title__in=member_names
+					).distinct().values_list('member_id', flat=True).count()
+					if name_match_count >= 2:
+						print("group match: %s (%d) matches %d on %d member names" % (author.name, author.janeway_id, candidate_releaser_id, name_match_count))
+						matching_releaser_ids.add(candidate_releaser_id)
+
 			else:
 				# get this author's group memberships
 				group_ids = Author.objects.filter(
 					is_group=True, member_memberships__member=author
 				).values_list('janeway_id', flat=True)
-				#group_names = [
-				#	generate_search_title(name.name)
-				#	for name in Name.objects.filter(author__is_group=True, author__member_memberships__member=author)
-				#]
+				group_names = [
+					generate_search_title(name.name)
+					for name in Name.objects.filter(author__is_group=True, author__member_memberships__member=author)
+				]
 
 				group_demozoo_ids = list(ReleaserExternalLink.objects.filter(
 					link_class='KestraBitworldAuthor', parameter__in=[str(id) for id in group_ids]
@@ -78,17 +84,22 @@ class Command(BaseCommand):
 					group_id__in=group_demozoo_ids
 				).distinct().values_list('member_id', flat=True))
 
-				# see if any candidate releasers have one or more matching groups by name
-				# - commented out due to false positives, e.g. JP/Mayhem:
+				# see if any candidate releasers have TWO or more matching groups by name
+				# (>=2 avoids false positives such as JP/Mayhem:
 				#   http://janeway.exotica.org.uk/author.php?id=29340
-				#   https://demozoo.org/sceners/40395/
-				#matching_releaser_ids_by_group_name = set(DZMembership.objects.filter(
-				#	member_id__in=candidate_releaser_ids,
-				#	group__nicks__variants__search_title__in=group_names
-				#).distinct().values_list('member_id', flat=True))
+				#   https://demozoo.org/sceners/40395/ )
+				for candidate_releaser_id in candidate_releaser_ids:
+					if candidate_releaser_id in matching_releaser_ids:
+						# ignore ones which are already matched by member ID
+						continue
 
-				#if matching_releaser_ids:
-				#	print("Found match for %s (%d): %r" % (author.name, author.janeway_id, matching_releaser_ids))
+					name_match_count = DZMembership.objects.filter(
+						member_id=candidate_releaser_id,
+						group__nicks__variants__search_title__in=group_names
+					).distinct().values_list('group_id', flat=True).count()
+					if name_match_count >= 2:
+						print("scener match: %s (%d) matches %d on %d group names" % (author.name, author.janeway_id, candidate_releaser_id, name_match_count))
+						matching_releaser_ids.add(candidate_releaser_id)
 
 			if len(matching_releaser_ids) > 1:
 				dupes_csv.writerow(
