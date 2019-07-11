24dcd88513847edc11ffa81c8221195e6ac7e556
Matt Westcott <matt@west.co.tt>
2019-07-12 01:08:39 +0200

    Add release/credit models to janeway

--- janeway/models.py
+++ janeway/models.py
@@ -25,3 +25,30 @@ class Membership(models.Model):
 	since = models.DateField(blank=True, null=True)
 	until = models.DateField(blank=True, null=True)
 	founder = models.BooleanField(default=False)
+
+
+SUPERTYPE_CHOICES = (
+	('production', 'Production'),
+	('graphics', 'Graphics'),
+	('music', 'Music'),
+)
+
+
+class Release(models.Model):
+	janeway_id = models.IntegerField()
+	title = models.CharField(max_length=255)
+	supertype = models.CharField(max_length=20, choices=SUPERTYPE_CHOICES)
+	author_names = models.ManyToManyField(Name, related_name='authored_releases')
+
+
+class ReleaseType(models.Model):
+	release = models.ForeignKey(Release, on_delete=models.CASCADE, related_name='types')
+	type_name = models.CharField(max_length=255)
+
+
+class Credit(models.Model):
+	janeway_id = models.IntegerField()
+	release = models.ForeignKey(Release, on_delete=models.CASCADE, related_name='credits')
+	name = models.ForeignKey(Name, on_delete=models.CASCADE, related_name='credits')
+	category = models.CharField(max_length=50)
+	description = models.CharField(max_length=255, blank=True)
