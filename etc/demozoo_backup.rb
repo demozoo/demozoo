a56ad5d510b8cf1db6d69364f2c814fb4313dbfd
Matt Westcott <matt@west.co.tt>
2019-08-02 01:01:32 +0100

    Update db backup scripts to point to db1 server

--- etc/demozoo_backup.rb
+++ etc/demozoo_backup.rb
@@ -2,7 +2,7 @@
 
 require 'rubygems'
 
-system "pg_dump -c demozoo | gzip - > /home/demozoo/demozoo/data/demozoo.sql.gz"
+system "pg_dump -h db1 -w -c demozoo | gzip - > /home/demozoo/demozoo/data/demozoo.sql.gz"
 system "s3cmd put /home/demozoo/demozoo/data/demozoo.sql.gz s3://demozoo-backups/hourly/demozoo-hourly-`date +%H`.sql.gz"
 if Time.now.hour == 2
         system "s3cmd put /home/demozoo/demozoo/data/demozoo.sql.gz s3://demozoo-backups/daily/demozoo-daily-`date +%Y-%m-%d`.sql.gz"
--- etc/demozoo_export.sh
+++ etc/demozoo_export.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-pg_dump -c demozoo -f /home/demozoo/demozoo/data/demozoo-export-raw.sql
-psql demozoo_export -f /home/demozoo/demozoo/data/demozoo-export-raw.sql
-psql demozoo_export -c "UPDATE auth_user SET email='', password='"'!'"', first_name='', last_name='';"
-psql demozoo_export -c "UPDATE demoscene_releaser SET first_name='' WHERE show_first_name='f';"
-psql demozoo_export -c "UPDATE demoscene_releaser SET surname='' WHERE show_surname='f';"
-psql demozoo_export -c "UPDATE productions_production SET search_document=NULL;"
-psql demozoo_export -c "UPDATE demoscene_releaser SET search_document=NULL, admin_search_document=NULL;"
-psql demozoo_export -c "UPDATE parties_party SET search_document=NULL;"
-psql demozoo_export -c "TRUNCATE TABLE celery_taskmeta, celery_tasksetmeta, django_session, djcelery_intervalschedule, djcelery_periodictask, djcelery_crontabschedule, djcelery_periodictasks, djcelery_taskstate, djcelery_workerstate;"
-psql demozoo_export -c "TRUNCATE TABLE janeway_author, janeway_credit, janeway_downloadlink, janeway_membership, janeway_name, janeway_packcontent, janeway_release, janeway_release_author_names, janeway_releasetype, janeway_soundtracklink;"
+pg_dump -h db1 -w -c demozoo -f /home/demozoo/demozoo/data/demozoo-export-raw.sql
+psql -h db1 -w demozoo_export -f /home/demozoo/demozoo/data/demozoo-export-raw.sql
+psql -h db1 -w demozoo_export -c "UPDATE auth_user SET email='', password='"'!'"', first_name='', last_name='';"
+psql -h db1 -w demozoo_export -c "UPDATE demoscene_releaser SET first_name='' WHERE show_first_name='f';"
+psql -h db1 -w demozoo_export -c "UPDATE demoscene_releaser SET surname='' WHERE show_surname='f';"
+psql -h db1 -w demozoo_export -c "UPDATE productions_production SET search_document=NULL;"
+psql -h db1 -w demozoo_export -c "UPDATE demoscene_releaser SET search_document=NULL, admin_search_document=NULL;"
+psql -h db1 -w demozoo_export -c "UPDATE parties_party SET search_document=NULL;"
+psql -h db1 -w demozoo_export -c "TRUNCATE TABLE celery_taskmeta, celery_tasksetmeta, django_session, djcelery_intervalschedule, djcelery_periodictask, djcelery_crontabschedule, djcelery_periodictasks, djcelery_taskstate, djcelery_workerstate;"
+psql -h db1 -w demozoo_export -c "TRUNCATE TABLE janeway_author, janeway_credit, janeway_downloadlink, janeway_membership, janeway_name, janeway_packcontent, janeway_release, janeway_release_author_names, janeway_releasetype, janeway_soundtracklink;"
 
-pg_dump -O demozoo_export | gzip - > /home/demozoo/demozoo/data/demozoo-export.sql.gz
+pg_dump -h db1 -w -O demozoo_export | gzip - > /home/demozoo/demozoo/data/demozoo-export.sql.gz
 
 s3cmd put -P /home/demozoo/demozoo/data/demozoo-export.sql.gz s3://data.demozoo.org/demozoo-export.sql.gz
