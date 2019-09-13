3e725373cf4c8f472776a71510b4e6165ac37540
Matt Westcott <matt@west.co.tt>
2019-09-13 18:15:36 +0100

    Remove South migrations (and BlobField)

--- lib/blob_field.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class BlobField(models.Field):
-	description = "Blob"
-	def db_type(self, connection):
-		return 'bytea' # only valid for postgres!
-
-from south.modelsinspector import add_introspection_rules
-add_introspection_rules([], ["^blob_field.BlobField"])
