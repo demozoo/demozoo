41da884ad1893a3e4752cf3496ecb14e6258ba36
Matt Westcott <matt@west.co.tt>
2019-09-18 00:53:40 +0100

    Add tests for releaser locking

--- demoscene/tests/test_releaser_views.py
+++ demoscene/tests/test_releaser_views.py
@@ -368,3 +368,51 @@ class TestEditExternalLinks(TestCase):
             ReleaserExternalLink.objects.filter(releaser=self.gasman, link_class='TwitterAccount').count(),
             1
         )
+
+
+class TestLocking(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='12345')
+        User.objects.create_superuser(username='testsuperuser', email='testsuperuser@example.com', password='12345')
+        self.gasman = Releaser.objects.get(name='Gasman')
+        self.papaya_dezign = Releaser.objects.get(name='Papaya Dezign')
+
+    def test_not_superuser(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get('/releasers/%d/lock/' % self.gasman.id)
+        self.assertRedirects(response, '/sceners/%d/' % self.gasman.id)
+
+    def test_get_lock(self):
+        self.client.login(username='testsuperuser', password='12345')
+        response = self.client.get('/releasers/%d/lock/' % self.gasman.id)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_lock(self):
+        self.client.login(username='testsuperuser', password='12345')
+        response = self.client.post('/releasers/%d/lock/' % self.gasman.id, {
+            'yes': 'yes'
+        })
+        self.assertRedirects(response, '/sceners/%d/' % self.gasman.id)
+        self.assertTrue(Releaser.objects.get(name='Gasman').locked)
+
+    def test_get_protected_view(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get('/releasers/%d/protected/' % self.papaya_dezign.id)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_unlock(self):
+        self.client.login(username='testsuperuser', password='12345')
+        response = self.client.post('/releasers/%d/protected/' % self.papaya_dezign.id, {
+            'yes': 'yes'
+        })
+        self.assertRedirects(response, '/groups/%d/' % self.papaya_dezign.id)
+        self.assertFalse(Releaser.objects.get(name='Papaya Dezign').locked)
+
+    def test_non_superuser_cannot_unlock(self):
+        self.client.login(username='testuser', password='12345')
+        self.client.post('/releasers/%d/protected/' % self.papaya_dezign.id, {
+            'yes': 'yes'
+        })
+        self.assertTrue(Releaser.objects.get(name='Papaya Dezign').locked)
