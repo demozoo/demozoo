b979b5ed12f131b08c51b744f84939f5a9e531d2
Matt Westcott <matt@west.co.tt>
2019-09-29 19:21:06 +0100

    Add tests for platform views

---
+++ platforms/tests/test_views.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.test import TestCase
+
+from platforms.models import Platform
+
+
+class TestShowIndex(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def test_get(self):
+        response = self.client.get('/platforms/')
+        self.assertEqual(response.status_code, 200)
+
+
+class TestShowPlatform(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def test_get(self):
+        platform = Platform.objects.get(name='ZX Spectrum')
+        response = self.client.get('/platforms/%d/' % platform.id)
+        self.assertEqual(response.status_code, 200)
