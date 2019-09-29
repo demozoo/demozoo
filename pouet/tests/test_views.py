b744531818d7bfac266395dfd5f61c83b046b654
Matt Westcott <matt@west.co.tt>
2019-09-29 20:28:49 +0100

    Add tests for Pouet matching views

---
+++ pouet/tests/test_views.py
@@ -0,0 +1,67 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from demoscene.models import Releaser
+from productions.models import Production
+
+
+class TestGroupsView(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+    def test_get(self):
+        response = self.client.get('/pouet/groups/')
+        self.assertEqual(response.status_code, 200)
+
+
+class TestMatchGroup(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+    def test_get(self):
+        hprg = Releaser.objects.get(name='Hooy-Program')
+        response = self.client.get('/pouet/groups/%d/' % hprg.id)
+        self.assertEqual(response.status_code, 200)
+
+
+class TestProductionLink(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+    def test_post(self):
+        pondlife = Production.objects.get(title="Pondlife")
+        response = self.client.post('/pouet/production-link/', {
+            'demozoo_id': pondlife.id,
+            'pouet_id': '2611',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(pondlife.links.filter(link_class='PouetProduction', parameter='2611').exists())
+
+
+class TestProductionUnink(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+    def test_post(self):
+        pondlife = Production.objects.get(title="Pondlife")
+        pondlife.links.create(link_class='PouetProduction', parameter='2611', is_download_link=False)
+        response = self.client.post('/pouet/production-unlink/', {
+            'demozoo_id': pondlife.id,
+            'pouet_id': '2611',
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(pondlife.links.filter(link_class='PouetProduction', parameter='2611').exists())
