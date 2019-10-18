e8a06ca1b41938cde13dcd9b506e49370620ff77
Matt Westcott <matt@west.co.tt>
2019-10-18 21:05:00 +0200

    Add tests for search views

---
+++ users/tests/test_views.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import, unicode_literals
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+
+class TestUsersIndex(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def test_get(self):
+        User.objects.create_superuser(username='testsuperuser', email='testsuperuser@example.com', password='12345')
+        self.client.login(username='testsuperuser', password='12345')
+        response = self.client.get('/users/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_get_non_superuser(self):
+        User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get('/users/')
+        self.assertRedirects(response, '/')
+
+
+class TestShowUser(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def test_get(self):
+        testuser = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testsuperuser', password='12345')
+        response = self.client.get('/users/%d/' % testuser.id)
+        self.assertEqual(response.status_code, 200)
