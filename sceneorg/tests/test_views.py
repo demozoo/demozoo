4c9b472096acd48d045e5b82ab9331fbcf01add3
Matt Westcott <matt@west.co.tt>
2019-10-18 19:07:36 +0200

    Add tests for scene.org compofolder link/unlink views

--- sceneorg/tests/test_views.py
+++ sceneorg/tests/test_views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 
 from parties.models import Party
+from sceneorg.models import Directory
 
 
 class TestCompoFolders(TestCase):
@@ -35,3 +36,42 @@ class TestCompoFolderParty(TestCase):
     def test_get(self):
         response = self.client.get('/sceneorg/compofolders/party/%d/' % self.forever2e3.id)
         self.assertEqual(response.status_code, 200)
+
+
+class TestCompoFolderLink(TestCase):
+    fixtures = ['tests/sceneorg.json', 'tests/gasman.json']
+
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+    def test_post(self):
+        forever2e3 = Party.objects.get(name='Forever 2e3')
+        zx1k = forever2e3.competitions.get(name='ZX 1K Intro')
+        response = self.client.post('/sceneorg/compofolders/link/', {
+            'directory_id': Directory.objects.get(path='/parties/2000/forever00/zx_demo/').id,
+            'competition_id': zx1k.id
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(zx1k.sceneorg_directories.filter(path='/parties/2000/forever00/zx_demo/').exists())
+
+
+class TestCompoFolderUnLink(TestCase):
+    fixtures = ['tests/sceneorg.json', 'tests/gasman.json']
+
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+
+    def test_post(self):
+        forever2e3 = Party.objects.get(name='Forever 2e3')
+        zx1k = forever2e3.competitions.get(name='ZX 1K Intro')
+        directory = Directory.objects.get(path='/parties/2000/forever00/zx_demo/')
+        zx1k.sceneorg_directories.add(directory)
+
+        response = self.client.post('/sceneorg/compofolders/unlink/', {
+            'directory_id': directory.id,
+            'competition_id': zx1k.id
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(zx1k.sceneorg_directories.filter(path='/parties/2000/forever00/zx_demo/').exists())
