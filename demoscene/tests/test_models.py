4a4eb92d1bbff3e0e71f3bef779096a9cb320f64
Matt Westcott <matt@west.co.tt>
2019-10-19 17:17:09 +0200

    Additional test coverage for demoscene models

--- demoscene/tests/test_models.py
+++ demoscene/tests/test_models.py
@@ -1,6 +1,5 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-import datetime
 
 from django.test import TestCase
 
@@ -13,6 +12,8 @@ class TestReleaser(TestCase):
         self.gasman = Releaser.objects.create(
             name="Gasman",
             is_group=False,
+            location="Århus, Denmark",
+            notes="arrest this man, he talks in <i>maths</i>",
         )
         self.hooy_program = Releaser.objects.create(
             name="Hooy-Program",
@@ -58,6 +59,43 @@ class TestReleaser(TestCase):
             self.hooy_program.get_history_url(),
             '/groups/%d/history/' % self.hooy_program.id
         )
+
+    def test_asciified_location(self):
+        self.assertEqual(self.gasman.asciified_location, "Arhus, Denmark")
+
+    def test_plaintext_notes(self):
+        self.assertEqual(self.gasman.plaintext_notes, "arrest this man, he talks in maths")
+
+
+class TestRealName(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def test_full_name(self):
+        gasman = Releaser.objects.get(name="Gasman")
+        gasman.show_surname = True
+        self.assertEqual(gasman.public_real_name, "Matt Westcott")
+
+    def test_surname_only(self):
+        gasman = Releaser.objects.get(name="Gasman")
+        gasman.show_surname = True
+        gasman.show_first_name = False
+        self.assertEqual(gasman.public_real_name, "Westcott")
+
+    def test_no_name(self):
+        gasman = Releaser.objects.get(name="Gasman")
+        gasman.show_surname = False
+        gasman.show_first_name = False
+        self.assertEqual(gasman.public_real_name, None)
+
+    def test_asciified_real_name(self):
+        gasman = Releaser.objects.get(name="Gasman")
+        gasman.first_name = "Bjørn"
+        self.assertEqual(gasman.asciified_real_name, "Bjorn Westcott")
+
+    def test_asciified_public_real_name(self):
+        gasman = Releaser.objects.get(name="Gasman")
+        gasman.first_name = "Bjørn"
+        self.assertEqual(gasman.asciified_public_real_name, "Bjorn")
 
 
 class TestReleaserProductions(TestCase):
@@ -177,6 +215,22 @@ class TestReleaserNicks(TestCase):
         with self.assertNumQueries(0):
             self.assertEqual(gasman.primary_nick.name, "Gasman")
 
+    def test_bad_primary_nick(self):
+        gasman = Releaser.objects.filter(name="Gasman").prefetch_related('nicks').first()
+        gasman.name = 'not gasman'
+        with self.assertRaises(Nick.DoesNotExist):
+            gasman.primary_nick
+
+    def test_multiple_primary_nicks(self):
+        Nick.objects.create(releaser=Releaser.objects.get(name="Gasman"), name="not gasman")
+        gasman = Releaser.objects.filter(name="Gasman").prefetch_related('nicks').first()
+        for nick in gasman.nicks.all():
+            if nick.name == 'not gasman':
+                nick.name = 'Gasman'
+
+        with self.assertRaises(Nick.MultipleObjectsReturned):
+            gasman.primary_nick
+
     def test_abbreviation(self):
         raww_arse = Releaser.objects.get(name="Raww Arse")
         self.assertEqual(raww_arse.abbreviation, "RA")
@@ -188,3 +242,51 @@ class TestReleaserNicks(TestCase):
         gasman = Releaser.objects.get(name="Gasman")
         gasman_nicks = sorted([nick.name for nick in gasman.alternative_nicks])
         self.assertEqual(gasman_nicks, ["Shingebis"])
+
+    def test_all_names_string(self):
+        gasman = Releaser.objects.get(name="Gasman")
+        self.assertEqual(gasman.all_names_string, "Gasman, Shingebis")
+
+    def test_all_names_string_prefetched(self):
+        gasman = Releaser.objects.filter(name="Gasman").prefetch_related('nicks__variants').first()
+        with self.assertNumQueries(0):
+            self.assertEqual(gasman.all_names_string, "Gasman, Shingebis")
+
+    def test_asciified_all_names_string(self):
+        gasman = Releaser.objects.get(name="Gasman")
+        gasman.primary_nick.name = "Gåsman"
+        self.assertEqual(gasman.asciified_all_names_string, "Gasman, Shingebis")
+
+    def test_all_affiliation_names_string(self):
+        gasman = Releaser.objects.get(name="Gasman")
+        self.assertEqual(gasman.all_affiliation_names_string, "Hooy-Program, Raww Arse")
+
+    def test_nick_from_id_and_name(self):
+        okkie = Nick.from_id_and_name("newscener", "Okkie")
+        self.assertEqual(okkie.name, "Okkie")
+        self.assertFalse(okkie.releaser.is_group)
+
+        poobrain = Nick.from_id_and_name("newgroup", "Poo-Brain")
+        self.assertEqual(poobrain.name, "Poo-Brain")
+        self.assertTrue(poobrain.releaser.is_group)
+
+        gasman = Nick.from_id_and_name(Nick.objects.get(name="Gasman").id, "Gasman")
+        self.assertEqual(gasman.name, "Gasman")
+        self.assertFalse(gasman.releaser.is_group)
+
+    def test_rewrite_nick_variant_list(self):
+        gasman = Releaser.objects.get(name="Gasman").primary_nick
+        gasman.nick_variant_list = "Gasman, GasBloke"
+        self.assertEqual(gasman.nick_variant_list, "Gasman, GasBloke")
+
+    def test_changing_primary_nick_changes_releaser_name(self):
+        gasman_nick = Releaser.objects.get(name="Gasman").primary_nick
+        gasman_nick.name = "GasBloke"
+        gasman_nick.save()
+        self.assertTrue(Releaser.objects.filter(name="GasBloke").exists())
+
+    def test_cannot_delete_primary_nick_by_reassignment(self):
+        gasman_nick = Releaser.objects.get(name="Gasman").primary_nick
+        with self.assertRaises(Exception):
+            gasman_nick.reassign_references_and_delete()
+        self.assertTrue(Nick.objects.filter(name="Gasman").exists())
