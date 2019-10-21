37f25bdccb241fca572b4619647fb97582a62b7e
Matt Westcott <matt@west.co.tt>
2019-10-21 14:52:57 +0200

    Remove unused queryset clause from json template filter

--- demoscene/templatetags/json_filter.py
+++ demoscene/templatetags/json_filter.py
@@ -1,15 +1,11 @@
 import json
 
-from django.core.serializers import serialize
-from django.db.models.query import QuerySet
 from django.template import Library
 
 register = Library()
 
 
 def jsonify(object):
-    if isinstance(object, QuerySet):
-        return serialize('json', object)
     return json.dumps(object)
 
 register.filter('json', jsonify)
