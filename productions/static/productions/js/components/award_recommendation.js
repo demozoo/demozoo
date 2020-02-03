78eff0d5b7bf10d00128fead3fd4edc860be5bc4
Matt Westcott <matt@west.co.tt>
2020-02-03 20:11:38 +0000

    add styling/js for award recommendations panel

---
+++ productions/static/productions/js/components/award_recommendation.js
@@ -0,0 +1,16 @@
+$(function() {
+    $('.award-recommendation').each(function() {
+        var container = $(this);
+        var header = container.find('.header');
+        var inner = container.find('.inner');
+        container.addClass('closed expandable');
+
+        header.click(function() {
+            if (container.hasClass('closed')) {
+                inner.slideDown(function() {container.removeClass('closed');});
+            } else {
+                inner.slideUp('fast', function() {container.addClass('closed');});
+            }
+        })
+    });
+});
