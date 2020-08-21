4c9743fa59c34adb92d27dabb81fbfef217efe22
Matt Westcott <matt@west.co.tt>
2020-08-22 00:43:06 +0100

    ignore things from test coverage: non-test settings files, fabfile, debug urls

--- demozoo/urls.py
+++ demozoo/urls.py
@@ -97,7 +97,7 @@ urlpatterns += [
     url(r'^awards/', include('awards.urls')),
 ]
 
-if settings.DEBUG and settings.DEBUG_TOOLBAR_ENABLED:
+if settings.DEBUG and settings.DEBUG_TOOLBAR_ENABLED:  # pragma: no cover
     import debug_toolbar
     urlpatterns = [
         url(r'^__debug__/', include(debug_toolbar.urls)),
