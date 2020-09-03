1d592ac5fe54b2b18e19a712c79d306bdabaf481
Matt Westcott <matt@west.co.tt>
2020-09-03 01:31:28 +0100

    Add test coverage for model_thumbnail

---
+++ platforms/tests/test_models.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import, unicode_literals
+
+from io import BytesIO
+
+from django.core.files.images import ImageFile
+from django.test import TestCase
+import PIL.Image
+
+from platforms.models import Platform
+
+
+class TestPlatformModel(TestCase):
+    def test_create_thumbnail_landscape(self):
+        f = BytesIO()
+        image = PIL.Image.new('RGBA', (400, 200), 'white')
+        image.save(f, 'PNG')
+        image_file = ImageFile(f, name='test.png')
+
+        zx = Platform.objects.create(
+            name="ZX Spectrum",
+            photo=image_file
+        )
+        self.assertEqual(zx.photo_width, 400)
+        self.assertEqual(zx.photo_height, 200)
+        self.assertIn('.png', zx.photo.name)
+        self.assertIn('.png', zx.thumbnail.name)
+        self.assertIn(zx.thumbnail_width, range(130, 140))
+        self.assertIn(zx.thumbnail_height, range(80, 100))
+
+    def test_create_thumbnail_portrait(self):
+        f = BytesIO()
+        image = PIL.Image.new('RGB', (240, 320), 'white')
+        image.save(f, 'JPEG')
+        image_file = ImageFile(f, name='test.jpg')
+
+        zx = Platform.objects.create(
+            name="ZX Spectrum",
+            photo=image_file
+        )
+        self.assertEqual(zx.photo_width, 240)
+        self.assertEqual(zx.photo_height, 320)
+        self.assertIn('.jpg', zx.photo.name)
+        self.assertIn('.jpg', zx.thumbnail.name)
+        self.assertIn(zx.thumbnail_width, range(130, 140))
+        self.assertIn(zx.thumbnail_height, range(80, 100))
