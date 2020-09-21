2d06f499320038c5b1954621181b0a96e449cbed
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2020-09-21 02:14:08 +0200

    Create a Dockerfile for the db
    Create a Dockerfile for the db and a polling entrypoint for the web so the database can be given time to bootstrap itself with migrations and whatnot.

---
+++ .docker/web/local.py
@@ -0,0 +1,13 @@
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+from .dev import DATABASES
+
+DATABASES['default']['USER'] = os.environ.get('POSTGRES_USER')
+DATABASES['default']['PASSWORD'] = os.environ.get('POSTGRES_PASSWORD')
+DATABASES['default']['HOST'] = os.environ.get('POSTGRES_HOST')
+
+AWS_STORAGE_BUCKET_NAME = 'media.demozoo.org'
+AWS_ACCESS_KEY_ID = 'get one from http://aws.amazon.com/s3/'
+AWS_SECRET_ACCESS_KEY = 'get one from http://aws.amazon.com/s3/'
