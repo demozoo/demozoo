3731e0e892b304541b32d991f53e282db90d7c87
Matt Westcott <matt@west.co.tt>
2020-10-05 21:25:50 +0100

    Complete test coverage for sceneorg.tasks

--- sceneorg/tests/test_tasks.py
+++ sceneorg/tests/test_tasks.py
@@ -7,7 +7,7 @@ from mock import patch
 from freezegun import freeze_time
 
 from sceneorg.models import Directory, File
-from sceneorg.tasks import fetch_new_sceneorg_files
+from sceneorg.tasks import fetch_new_sceneorg_files, scan_dir_listing
 
 
 class TestTasks(TestCase):
@@ -27,3 +27,33 @@ class TestTasks(TestCase):
 
     def test_nonsuccess_response(self):
         fetch_new_sceneorg_files(days=999)
+
+    @freeze_time('2020-02-02')
+    @patch('sceneorg.tasks.parse_all_dirs')
+    def test_scan_dir_listing(self, parse_all_dirs):
+        root_dir = Directory.objects.create(path='/', last_seen_at=datetime.datetime(2020, 1, 1))
+        demos_dir = Directory.objects.create(path='/demos/', parent=root_dir, last_seen_at=datetime.datetime(2020, 1, 1))
+        warez_dir = Directory.objects.create(path='/warez/', parent=root_dir, last_seen_at=datetime.datetime(2020, 1, 1))
+        games_dir = Directory.objects.create(path='/warez/games/', parent=warez_dir, last_seen_at=datetime.datetime(2020, 1, 1))
+        File.objects.create(path='/uploading.txt', directory=root_dir, last_seen_at=datetime.datetime(2020, 1, 1))
+        File.objects.create(path='/world-domination-plans.txt', directory=root_dir, last_seen_at=datetime.datetime(2020, 1, 1))
+
+        parse_all_dirs.return_value = [
+            ('/', [
+                ('demos', True, None),
+                ('graphics', True, None),
+                ('uploading.txt', False, '2129'),
+            ]),
+            ('/music/', [
+                ('cocio.xm', False, '440607'),
+            ]),
+        ]
+        scan_dir_listing()
+
+        self.assertEqual(Directory.objects.get(path='/demos/').last_seen_at, datetime.datetime(2020, 2, 2))
+        self.assertEqual(Directory.objects.get(path='/graphics/').last_seen_at, datetime.datetime(2020, 2, 2))
+        self.assertEqual(File.objects.get(path='/music/cocio.xm').last_seen_at, datetime.datetime(2020, 2, 2))
+        self.assertEqual(File.objects.get(path='/uploading.txt').last_seen_at, datetime.datetime(2020, 2, 2))
+        self.assertTrue(Directory.objects.get(path='/warez/').is_deleted)
+        self.assertTrue(Directory.objects.get(path='/warez/games/').is_deleted)
+        self.assertTrue(File.objects.get(path='/world-domination-plans.txt').is_deleted)
