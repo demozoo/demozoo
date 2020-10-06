2957422e0d0b894fb71861b45a3b159be58a6002
Matt Westcott <matt@west.co.tt>
2020-10-06 21:01:44 +0100

    Switch markdown cleaning from scrubber to html-sanitizer for py3 compatibility

--- demoscene/templatetags/safe_markdown.py
+++ demoscene/templatetags/safe_markdown.py
@@ -1,17 +1,41 @@
 from django import template
 from django.utils.safestring import mark_safe
 import markdown
-from scrubber import Scrubber
+from html_sanitizer import Sanitizer
 
 register = template.Library()
 
-scrubber = Scrubber(autolink=False, nofollow=False) # Scrubber's autolink doesn't handle ftp://
+
+def sanitize_href(href):
+    """
+    Verify that a given href is benign and allowed.
+    This is a stupid check, which probably should be much more elaborate
+    to be safe.
+    """
+    if href.startswith(("/", "mailto:", "http:", "https:", "#", "tel:", "ftp:")):
+        return href
+    return "#"
+
+
+sanitizer = Sanitizer({
+    'tags': (
+        'a', 'b', 'blockquote', 'br', 'center', 'code', 'del', 'em',
+        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'img', 'i', 'li',
+        'ol', 'pre', 'p', 'strike', 'strong', 'sup', 'sub', 'ul',
+    ),
+    'attributes': {
+        "a": ("href", "name", "title", "id", "rel"),
+        "img": ("src", "width", "height", "alt"),
+    },
+    'element_preprocessors': [],
+    'sanitize_href': sanitize_href,
+})
 md = markdown.Markdown(extensions=['nl2br', 'autolink'])
 
 @register.filter(is_safe=True)
 def safe_markdown(value, arg=''):
     return mark_safe(
-        scrubber.scrub(
+        sanitizer.sanitize(
             md.reset().convert(value)
         )
     )
--- homepage/tests/test_home_view.py
+++ homepage/tests/test_home_view.py
@@ -28,7 +28,7 @@ class SimpleTest(TestCase):
         )
         NewsStory.objects.create(
             title='First news item',
-            text='with a <a href="http://example.com/">link</a> in it\nand a line break',
+            text='''with a <a href="http://example.com/">link</a> in it\nand a <a href="javascript:alert('hacked by limp ninja')">line</a> break ftp://ftp.scene.org/pub/''',
             is_public=True,
         )
         NewsStory.objects.create(
@@ -51,7 +51,7 @@ class SimpleTest(TestCase):
         self.assertNotContains(response, 'Hello logged in people')
 
         self.assertContains(response, 'First news item')
-        self.assertContains(response, 'with a <a href="http://example.com/" class="external">link</a> in it<br />\nand a line break')
+        self.assertContains(response, 'with a <a href="http://example.com/">link</a> in it<br> and a <a href="#">line</a> break <a href="ftp://ftp.scene.org/pub/">ftp://ftp.scene.org/pub/</a>')
 
         self.assertNotContains(response, 'Secret news item')
 
@@ -65,9 +65,9 @@ class SimpleTest(TestCase):
         self.assertNotContains(response, 'Hello anonymous people')
         self.assertContains(response, 'Hello logged in people')
         self.assertContains(response, 'First news item')
-        self.assertContains(response, 'with a <a href="http://example.com/" class="external">link</a> in it')
+        self.assertContains(response, 'with a <a href="http://example.com/">link</a> in it')
         self.assertContains(response, 'Secret news item')
-        self.assertContains(response, 'wooo <a href="http://gasman.zxdemo.org/" class="external">fancy link</a>')
+        self.assertContains(response, 'wooo <a href="http://gasman.zxdemo.org/">fancy link</a>')
 
     @freeze_time('2018-12-30')
     def test_date_rollover(self):
