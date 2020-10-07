9a36cdc4fd6ce66bfb889848d65e22b7fe4af8b1
Matt Westcott <matt@west.co.tt>
2020-10-08 00:58:50 +0100

    replace iteritems with items

--- demoscene/templatetags/urlsub.py
+++ demoscene/templatetags/urlsub.py
@@ -8,7 +8,7 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def urlsub(context, **params_to_replace):
     params = context['request'].GET.copy()
-    for k, v in params_to_replace.iteritems():
+    for k, v in params_to_replace.items():
         # can't use params.update here, because with QueryDict that appends to the list
         # for each param rather than updating it. Grr.
         params[k] = v
