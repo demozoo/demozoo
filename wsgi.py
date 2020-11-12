b2c8990dfbf507efb9c83af2d36f12b53a10fa62
Matt Westcott <matt@west.co.tt>
2020-11-12 23:57:07 +0000

    Move DJANGO_SETTINGS_MODULE into .env and drop separate wsgi script for staging

--- wsgi.py
+++ wsgi.py
@@ -6,5 +6,4 @@ import dotenv
 from django.core.wsgi import get_wsgi_application
 
 dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'demozoo.settings.productionvm'
 application = get_wsgi_application()
