db99bc8569dcbb64915b38a9d300c7bb3cd29436
Matt Westcott <matt@west.co.tt>
2020-11-17 01:56:46 +0000

    replace demozoo_backup.rb with .env-based shell script

---
+++ etc/demozoo_backup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+. /home/demozoo/demozoo/.env
+export PGPASSWORD=$POSTGRES_PASSWORD
+
+pg_dump -h $POSTGRES_HOST -w -c demozoo | gzip - > /home/demozoo/demozoo/data/demozoo.sql.gz
+s3cmd --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY put /home/demozoo/demozoo/data/demozoo.sql.gz s3://demozoo-backups/hourly/demozoo-hourly-`date +%H`.sql.gz
+if [ $(date +%H) -eq 2 ] ; then
+    s3cmd --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY put /home/demozoo/demozoo/data/demozoo.sql.gz s3://demozoo-backups/daily/demozoo-daily-`date +%Y-%m-%d`.sql.gz
+fi
