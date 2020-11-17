0256403152a1b4c955912431c14e5d169470302f
Matt Westcott <matt@west.co.tt>
2020-11-17 01:25:13 +0000

    Update demozoo_export script to work with .env (but only on the new server)

--- etc/demozoo_export.sh
+++ etc/demozoo_export.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 
-pg_dump -h db1 -w -c demozoo -f /home/demozoo/demozoo/data/demozoo-export-raw.sql
-psql -h db1 -w demozoo_export -f /home/demozoo/demozoo/data/demozoo-export-raw.sql
-psql -h db1 -w demozoo_export -c "UPDATE auth_user SET email='', password='"'!'"', first_name='', last_name='';"
-psql -h db1 -w demozoo_export -c "UPDATE productions_production SET search_document=NULL;"
-psql -h db1 -w demozoo_export -c "UPDATE demoscene_releaser SET search_document=NULL;"
-psql -h db1 -w demozoo_export -c "UPDATE parties_party SET search_document=NULL;"
-psql -h db1 -w demozoo_export -c "UPDATE awards_event SET juror_feed_url='';"
-psql -h db1 -w demozoo_export -c "TRUNCATE TABLE celery_taskmeta, celery_tasksetmeta, django_session, djcelery_intervalschedule, djcelery_periodictask, djcelery_crontabschedule, djcelery_periodictasks, djcelery_taskstate, djcelery_workerstate;"
-psql -h db1 -w demozoo_export -c "TRUNCATE TABLE janeway_author, janeway_credit, janeway_downloadlink, janeway_membership, janeway_name, janeway_packcontent, janeway_release, janeway_release_author_names, janeway_releasetype, janeway_screenshot, janeway_soundtracklink;"
-psql -h db1 -w demozoo_export -c "TRUNCATE TABLE awards_recommendation, awards_juror;"
+. /home/demozoo/demozoo/.env
+export PGPASSWORD=$POSTGRES_PASSWORD
 
-pg_dump -h db1 -w -O demozoo_export | gzip - > /home/demozoo/demozoo/data/demozoo-export.sql.gz
+pg_dump -h $POSTGRES_HOST -w -c demozoo -f /home/demozoo/demozoo/data/demozoo-export-raw.sql
+psql -h $POSTGRES_HOST -w demozoo_export -f /home/demozoo/demozoo/data/demozoo-export-raw.sql
+psql -h $POSTGRES_HOST -w demozoo_export -c "UPDATE auth_user SET email='', password='"'!'"', first_name='', last_name='';"
+psql -h $POSTGRES_HOST -w demozoo_export -c "UPDATE productions_production SET search_document=NULL;"
+psql -h $POSTGRES_HOST -w demozoo_export -c "UPDATE demoscene_releaser SET search_document=NULL;"
+psql -h $POSTGRES_HOST -w demozoo_export -c "UPDATE parties_party SET search_document=NULL;"
+psql -h $POSTGRES_HOST -w demozoo_export -c "UPDATE awards_event SET juror_feed_url='';"
+psql -h $POSTGRES_HOST -w demozoo_export -c "TRUNCATE TABLE celery_taskmeta, celery_tasksetmeta, django_session, djcelery_intervalschedule, djcelery_periodictask, djcelery_crontabschedule, djcelery_periodictasks, djcelery_taskstate, djcelery_workerstate;"
+psql -h $POSTGRES_HOST -w demozoo_export -c "TRUNCATE TABLE janeway_author, janeway_credit, janeway_downloadlink, janeway_membership, janeway_name, janeway_packcontent, janeway_release, janeway_release_author_names, janeway_releasetype, janeway_screenshot, janeway_soundtracklink;"
+psql -h $POSTGRES_HOST -w demozoo_export -c "TRUNCATE TABLE awards_recommendation, awards_juror;"
 
-s3cmd put -P /home/demozoo/demozoo/data/demozoo-export.sql.gz s3://data.demozoo.org/demozoo-export.sql.gz
+pg_dump -h $POSTGRES_HOST -w -O demozoo_export | gzip - > /home/demozoo/demozoo/data/demozoo-export.sql.gz
+
+s3cmd --access_key=$AWS_ACCESS_KEY_ID --secret_key=$AWS_SECRET_ACCESS_KEY put -P /home/demozoo/demozoo/data/demozoo-export.sql.gz s3://data.demozoo.org/demozoo-export.sql.gz
