4767920fa2f8c4713814518b17cf4b68f5d4f54b
Matt Westcott <matt@west.co.tt>
2021-03-14 12:36:39 +0000

    add management command to only fetch embed data for links that don't have it

--- productions/management/commands/refetch_embed_data.py
+++ productions/management/commands/refetch_embed_data.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, unicode_literals
 
 from django.core.management.base import BaseCommand
 
+import datetime
+
 from demoscene.utils.groklinks import EMBEDDABLE_PRODUCTION_LINK_TYPES
 
 from productions.models import ProductionLink
@@ -10,17 +12,20 @@ from productions.tasks import fetch_production_link_embed_data
 
 class Command(BaseCommand):
     """
-    fetch all embed data that hasn't been fetched at all yet
+    fetch all embed data that hasn't been fetched in the last month
     """
     def handle(self, *args, **kwargs):
         link_types = [cls.__name__ for cls in EMBEDDABLE_PRODUCTION_LINK_TYPES]
+        last_month = datetime.date.today() - datetime.timedelta(days=30)
 
         # order by embed_data_last_fetch_time desc to fetch null ones (never fetched) first
         prod_links = ProductionLink.objects.filter(
             link_class__in=link_types
-        ).filter(
-            embed_data_last_fetch_time__isnull=True
-        )
+        ).exclude(
+            embed_data_last_fetch_time__gt=last_month
+        ).exclude(
+            embed_data_last_error_time__gt=last_month
+        ).order_by('-embed_data_last_fetch_time')
 
         for prod_link in prod_links:
             print("fetching embed data for %s" % prod_link.link)
--- productions/tests/test_commands.py
+++ productions/tests/test_commands.py
@@ -28,6 +28,26 @@ class TestFetchEmbedData(TestCase):
         fetch_production_link_embed_data.delay.assert_called_once_with(link.id)
 
 
+class TestRefetchEmbedData(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    @patch('productions.tasks.fetch_production_link_embed_data')
+    def test_run(self, fetch_production_link_embed_data):
+        pondlife = Production.objects.get(title='Pondlife')
+        # create with a bogus link_class to prevent fetch_production_link_embed_data
+        # from being called on save()
+        link = pondlife.links.create(
+            link_class='SpeccyWikiPage', parameter='1lFBXWxSrKE',
+            is_download_link=False
+        )
+        ProductionLink.objects.filter(id=link.id).update(link_class='YoutubeVideo')
+
+        with captured_stdout():
+            call_command('refetch_embed_data')
+
+        fetch_production_link_embed_data.delay.assert_called_once_with(link.id)
+
+
 class TestMirrorMusicFiles(TestCase):
     fixtures = ['tests/gasman.json']
 
