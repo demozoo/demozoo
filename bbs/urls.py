10eaee479638a12f7d1b8b2e9e2d26d1260cc72f
Matt Westcott <matt@west.co.tt>
2021-04-09 01:33:47 +0100

    Add show-text-ad view

--- bbs/urls.py
+++ bbs/urls.py
@@ -19,4 +19,5 @@ urlpatterns = [
     url(r'^(\d+)/edit_affiliation/(\d+)/$', views.edit_affiliation, {}, 'bbs_edit_affiliation'),
     url(r'^(\d+)/remove_affiliation/(\d+)/$', views.RemoveAffiliationView.as_view(), {}, 'bbs_remove_affiliation'),
     url(r'^(\d+)/edit_text_ads/$', views.EditTextAdsView.as_view(), {}, 'bbs_edit_text_ads'),
+    url(r'^(\d+)/text_ad/(\d+)/$', views.text_ad, {}, 'bbs_text_ad'),
 ]
