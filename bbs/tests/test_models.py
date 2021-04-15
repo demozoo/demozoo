c94c4386dbf2574f41d901a6a838525c0b3099e0
Matt Westcott <matt@west.co.tt>
2021-04-15 23:34:46 +0100

    Add BBS name model

--- bbs/tests/test_models.py
+++ bbs/tests/test_models.py
@@ -14,6 +14,23 @@ class TestBBS(TestCase):
         bbs = BBS.objects.get(name='StarPort')
         self.assertEqual(bbs.get_absolute_url(), '/bbs/%d/' % bbs.id)
 
+    def test_primary_name(self):
+        bbs = BBS.objects.get(name='StarPort')
+        self.assertEqual(bbs.primary_name.name, 'StarPort')
+        self.assertEqual(str(bbs.primary_name), 'StarPort')
+        self.assertTrue(bbs.primary_name.is_primary_name())
+
+    def test_asciified_name(self):
+        bbs = BBS.objects.get(name='StarPort')
+        self.assertEqual(bbs.asciified_name, 'StarPort')
+
+    def test_update_name(self):
+        bbs = BBS.objects.get(name='StarPort')
+        name = bbs.primary_name
+        name.name = 'also starport'
+        name.save()
+        self.assertEqual(bbs.name, 'also starport')
+
 
 class TestOperator(TestCase):
     fixtures = ['tests/gasman.json']
