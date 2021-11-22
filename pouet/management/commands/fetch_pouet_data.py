ff1ff41406867bab31769d5b937360a66d9051c0
Matt Westcott <matt@west.co.tt>
2021-11-22 23:16:38 +0000

    Import prods from pouet data dump

--- pouet/management/commands/fetch_pouet_data.py
+++ pouet/management/commands/fetch_pouet_data.py
@@ -5,7 +5,7 @@ import json
 import requests
 from django.core.management.base import BaseCommand
 
-from pouet.models import Group
+from pouet.models import Group, Production
 
 
 class Command(BaseCommand):
@@ -18,12 +18,15 @@ class Command(BaseCommand):
         wednesday = today - datetime.timedelta(days=days_since_wednesday)
         datestamp = wednesday.strftime('%Y%m%d')
 
+        print("importing groups...")
         groups_url = "https://data.pouet.net/dumps/202111/pouetdatadump-groups-%s.json.gz" % datestamp
         r = requests.get(groups_url, stream=True)
         groups_file = gzip.GzipFile(fileobj=r.raw)
 
         groups_imported = 0
         groups_created = 0
+
+        group_db_ids = {}
 
         def handle_group(group_data):
             nonlocal groups_imported, groups_created
@@ -33,6 +36,7 @@ class Command(BaseCommand):
                     'demozoo_id': group_data['demozoo'],
                     'last_seen_at': datetime.datetime.now(),
                 })
+                group_db_ids[group_data['id']] = group.id
                 groups_imported += 1
                 if groups_imported % 1000 == 0:
                     print("%d groups imported" % groups_imported)
@@ -42,4 +46,38 @@ class Command(BaseCommand):
 
         json.load(groups_file, object_hook=handle_group)
         groups_file.close()
-        print("done: %d groups imported, of which %d newly created" % (groups_imported, groups_created))
+        print("done. %d groups imported, of which %d newly created" % (groups_imported, groups_created))
+
+        print("importing prods...")
+        prods_url = "https://data.pouet.net/dumps/202111/pouetdatadump-prods-%s.json.gz" % datestamp
+        r = requests.get(prods_url, stream=True)
+        prods_file = gzip.GzipFile(fileobj=r.raw)
+
+        prods_imported = 0
+        prods_created = 0
+
+        def handle_prod(prod_data):
+            nonlocal prods_imported, prods_created, group_db_ids
+            # prods JSON contains various nested objects, but only prod entries have a 'download' field
+            if 'download' in prod_data:
+                prod, created = Production.objects.update_or_create(pouet_id=prod_data['id'], defaults={
+                    'name': prod_data['name'],
+                    'last_seen_at': datetime.datetime.now(),
+                })
+                prod.groups.set([
+                    group_db_ids[group['id']]
+                    for group in prod_data['groups']
+                ])
+
+                prods_imported += 1
+                if prods_imported % 1000 == 0:
+                    print("%d prods imported" % prods_imported)
+
+                if created:
+                    prods_created += 1
+
+            return prod_data
+
+        json.load(prods_file, object_hook=handle_prod)
+        prods_file.close()
+        print("done. %d prods imported, of which %d newly created" % (prods_imported, prods_created))
