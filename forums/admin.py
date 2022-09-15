02c0de73fb891d0452c344e438a7f9be722f2713
Matt Westcott <matt@west.co.tt>
2022-09-15 12:32:43 +0100

    use raw_id_fields for users on forum admin

--- forums/admin.py
+++ forums/admin.py
@@ -5,10 +5,12 @@ from forums.models import Post, Topic
 
 class PostInline(admin.TabularInline):
     model = Post
+    raw_id_fields = ['user']
 
 
 admin.site.register(
     Topic,
     inlines=[PostInline],
-    search_fields=['title']
+    search_fields=['title'],
+    raw_id_fields=['created_by_user', 'last_post_by_user']
 )
