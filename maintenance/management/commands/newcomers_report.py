fe4507ba56fb79273923d9dfb7283b7bb44d9409
Matt Westcott <matt@west.co.tt>
2023-01-13 20:49:48 +0000

    Add a management command to generate a 2022 newcomers report

---
+++ maintenance/management/commands/newcomers_report.py
@@ -0,0 +1,90 @@
+from django.core.management.base import BaseCommand
+from django.db import connection
+from django.db.models import Min
+
+from demoscene.models import Releaser
+
+
+class Command(BaseCommand):
+    help = "Generate a listing of candidates for the Meteoriks New Talent award"
+
+    def handle(self, *args, **kwargs):
+        with connection.cursor() as cursor:
+            cursor.execute("""
+SELECT DISTINCT releaser_id FROM (
+    SELECT DISTINCT demoscene_nick.releaser_id
+    FROM productions_production
+    INNER JOIN productions_production_author_nicks ON (
+        productions_production.id = productions_production_author_nicks.production_id
+    )
+    INNER JOIN demoscene_nick ON (
+        productions_production_author_nicks.nick_id = demoscene_nick.id
+    )
+    LEFT JOIN productions_production_types ON (
+        productions_production.id = productions_production_types.production_id
+    )
+    WHERE
+        EXTRACT(YEAR FROM release_date_date) = 2022
+        AND (
+            productions_production.supertype = 'production'
+            OR productions_production_types.productiontype_id IN (27, 28, 56)
+        )
+    UNION
+    SELECT DISTINCT demoscene_nick.releaser_id
+    FROM productions_production
+    INNER JOIN productions_production_author_affiliation_nicks ON (
+        productions_production.id = productions_production_author_affiliation_nicks.production_id
+    )
+    INNER JOIN demoscene_nick ON (
+        productions_production_author_affiliation_nicks.nick_id = demoscene_nick.id
+    )
+    LEFT JOIN productions_production_types ON (
+        productions_production.id = productions_production_types.production_id
+    )
+    WHERE
+        EXTRACT(YEAR FROM release_date_date) = 2022
+        AND (
+            productions_production.supertype = 'production'
+            OR productions_production_types.productiontype_id IN (27, 28, 56)
+        )
+    UNION
+    SELECT DISTINCT demoscene_nick.releaser_id
+    FROM productions_production
+    INNER JOIN productions_credit ON (
+        productions_production.id = productions_credit.production_id
+    )
+    INNER JOIN demoscene_nick ON (
+        productions_credit.nick_id = demoscene_nick.id
+    )
+    LEFT JOIN productions_production_types ON (
+        productions_production.id = productions_production_types.production_id
+    )
+    WHERE
+        EXTRACT(YEAR FROM release_date_date) = 2022
+        AND (
+            productions_production.supertype = 'production'
+            OR productions_production_types.productiontype_id IN (27, 28, 56)
+        )
+) AS releasers_this_year;
+            """)
+            releasers_this_year = [
+                releaser_id
+                for releaser_id, in cursor.fetchall()
+            ]
+
+        for releaser_id in releasers_this_year:
+            releaser = Releaser.objects.get(id=releaser_id)
+
+            author_rel_date = releaser.productions().aggregate(rel_date=Min('release_date_date'))['rel_date']
+            if author_rel_date and author_rel_date.year < 2022:
+                continue
+
+            affil_rel_date = releaser.member_productions().aggregate(rel_date=Min('release_date_date'))['rel_date']
+            if affil_rel_date and affil_rel_date.year < 2022:
+                continue
+
+            credit_rel_date = releaser.credits().aggregate(rel_date=Min('production__release_date_date'))['rel_date']
+            if credit_rel_date and credit_rel_date.year < 2022:
+                continue
+
+            print("%s,https://demozoo.org%s" % (releaser.name, releaser.get_absolute_url()))
