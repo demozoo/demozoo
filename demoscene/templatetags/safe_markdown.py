ee10c7cef2e7c85cf6e64ade1019d95f7a2e7007
Nuey San Waldman <nswaldman@gmail.com>
2023-08-27 23:57:30 +0200

    add footnote extension, whitelist required elements and attributes.

--- demoscene/templatetags/safe_markdown.py
+++ demoscene/templatetags/safe_markdown.py
@@ -10,25 +10,28 @@ register = template.Library()
 
 cleaner = Cleaner(
     tags=[
-        'a', 'b', 'blockquote', 'br', 'center', 'code', 'del', 'em',
-        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'img', 'i', 'li',
-        'ol', 'pre', 'p', 'strike', 'strong', 'sup', 'sub', 'ul',
+        'a', 'b', 'blockquote', 'br', 'center', 'code', 'del', 'div',
+        'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'img', 'i',
+        'li', 'ol', 'pre', 'p', 'strike', 'strong', 'sup', 'sub', 'ul',
     ],
     attributes={
         "a": ("href", "name", "title", "id", "rel"),
+        "div": ("class"),
         "img": ("src", "width", "height", "alt"),
+        "li": ("id"),
+        "sup": ("id"),
     },
     strip=True,
     protocols=['http', 'https', 'mailto', 'ftp', 'tel'],
 )
 autolink = AutoLinkExtension()
-md = markdown.Markdown(extensions=['nl2br', autolink])
+md = markdown.Markdown(extensions=['footnotes', 'nl2br', autolink])
 
 
 @register.filter(is_safe=True)
 def safe_markdown(value, arg=''):
     return mark_safe(
         cleaner.clean(
-            md.reset().convert(value)
+        md.reset().convert(value)
         )
     )
