b16670a2575dece15485e818b4872363abce7615
Matt Westcott <matt@west.co.tt>
2023-11-01 01:02:03 +0000

    Add test coverage for customised admin views

---
+++ productions/tests/test_admin.py
@@ -0,0 +1,21 @@
+import json
+import os
+from io import BytesIO
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+
+
+class TestProductionAdmin(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def setUp(self):
+        User.objects.create_superuser(username='testsuperuser', email='testsuperuser@example.com', password='12345')
+        self.client.login(username='testsuperuser', password='12345')
+
+    def test_get(self):
+        response = self.client.get('/admin/productions/production/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Madrielle")
+        self.assertContains(response, "Gasman / Raww Arse")
