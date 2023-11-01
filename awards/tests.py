2a9364d9d230fdb05dac0647b817f1dc8c2e146b
Matt Westcott <matt@west.co.tt>
2023-11-01 01:02:03 +0000

    Add test coverage for customised admin views

--- awards/tests.py
+++ awards/tests.py
@@ -264,3 +264,17 @@ class TestFetchJurors(TestCase):
 
         self.assertEqual(Juror.objects.filter(user=self.testuser).count(), 1)
         self.assertEqual(Juror.objects.filter(user=self.truck).count(), 1)
+
+
+class TestAwardsAdmin(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def setUp(self):
+        User.objects.create_superuser(username='testsuperuser', email='testsuperuser@example.com', password='12345')
+        self.client.login(username='testsuperuser', password='12345')
+
+    def test_get(self):
+        response = self.client.get('/admin/awards/category/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "The Meteoriks 2020")
+        self.assertContains(response, "Best High-End Demo")
