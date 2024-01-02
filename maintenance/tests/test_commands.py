a90ec92a6cd794cd834787cb98aae6fc1ec68cb2
Matt Westcott <matt@west.co.tt>
2024-01-02 22:08:13 +0000

    add test coverage for newcomers report

---
+++ maintenance/tests/test_commands.py
@@ -0,0 +1,23 @@
+from django.core.management import call_command
+from django.test import TestCase
+from django.test.utils import captured_stdout
+
+from demoscene.models import Nick
+from productions.models import Production
+
+
+class TestNewcomersReport(TestCase):
+    fixtures = ['tests/gasman.json']
+
+    def test_run(self):
+        madrielle = Production.objects.get(title='Madrielle')
+        madrielle.author_affiliation_nicks.add(Nick.objects.get(name='Future Crew'))
+        madrielle.credits.create(nick=Nick.objects.get(name='Ra'), role='code')
+
+        brexecutable = Production.objects.get(title='The Brexecutable Music Compo Is Over')
+        brexecutable.credits.create(nick=Nick.objects.get(name='Abyss'), role='code')
+        brexecutable.credits.create(nick=Nick.objects.get(name='Future Crew'), role='code')
+        brexecutable.credits.create(nick=Nick.objects.get(name='Ra'), role='code')
+        with captured_stdout() as stdout:
+            call_command('newcomers_report', '2019')
+        self.assertEqual(stdout.getvalue(), "Abyss,https://demozoo.org/sceners/8/\n")
