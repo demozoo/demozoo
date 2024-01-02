1d99b11ed8f1135f0e14da4bfb91dd7ffaf756df
Matt Westcott <matt@west.co.tt>
2024-01-02 20:00:38 +0000

    make year a variable

--- maintenance/management/commands/newcomers_report.py
+++ maintenance/management/commands/newcomers_report.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandParser
 from django.db import connection
 from django.db.models import Min
 
@@ -8,7 +8,11 @@ from demoscene.models import Releaser
 class Command(BaseCommand):
     help = "Generate a listing of candidates for the Meteoriks New Talent award"
 
-    def handle(self, *args, **kwargs):
+    def add_arguments(self, parser):
+        parser.add_argument('year', type=int, help="Year to generate report for")
+
+    def handle(self, *args, **options):
+        year = options['year']
         with connection.cursor() as cursor:
             cursor.execute("""
 SELECT DISTINCT releaser_id FROM (
@@ -24,7 +28,7 @@ SELECT DISTINCT releaser_id FROM (
         productions_production.id = productions_production_types.production_id
     )
     WHERE
-        EXTRACT(YEAR FROM release_date_date) = 2022
+        EXTRACT(YEAR FROM release_date_date) = %(year)s
         AND (
             productions_production.supertype = 'production'
             OR productions_production_types.productiontype_id IN (27, 28, 56)
@@ -42,7 +46,7 @@ SELECT DISTINCT releaser_id FROM (
         productions_production.id = productions_production_types.production_id
     )
     WHERE
-        EXTRACT(YEAR FROM release_date_date) = 2022
+        EXTRACT(YEAR FROM release_date_date) = %(year)s
         AND (
             productions_production.supertype = 'production'
             OR productions_production_types.productiontype_id IN (27, 28, 56)
@@ -60,13 +64,13 @@ SELECT DISTINCT releaser_id FROM (
         productions_production.id = productions_production_types.production_id
     )
     WHERE
-        EXTRACT(YEAR FROM release_date_date) = 2022
+        EXTRACT(YEAR FROM release_date_date) = %(year)s
         AND (
             productions_production.supertype = 'production'
             OR productions_production_types.productiontype_id IN (27, 28, 56)
         )
 ) AS releasers_this_year;
-            """)
+            """, {'year': year})
             releasers_this_year = [
                 releaser_id
                 for releaser_id, in cursor.fetchall()
@@ -76,15 +80,15 @@ SELECT DISTINCT releaser_id FROM (
             releaser = Releaser.objects.get(id=releaser_id)
 
             author_rel_date = releaser.productions().aggregate(rel_date=Min('release_date_date'))['rel_date']
-            if author_rel_date and author_rel_date.year < 2022:
+            if author_rel_date and author_rel_date.year < year:
                 continue
 
             affil_rel_date = releaser.member_productions().aggregate(rel_date=Min('release_date_date'))['rel_date']
-            if affil_rel_date and affil_rel_date.year < 2022:
+            if affil_rel_date and affil_rel_date.year < year:
                 continue
 
             credit_rel_date = releaser.credits().aggregate(rel_date=Min('production__release_date_date'))['rel_date']
-            if credit_rel_date and credit_rel_date.year < 2022:
+            if credit_rel_date and credit_rel_date.year < year:
                 continue
 
             print("%s,https://demozoo.org%s" % (releaser.name, releaser.get_absolute_url()))
