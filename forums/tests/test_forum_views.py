fe61f64733c5c49411b4b180bcf862dcc57ced08
Matt Westcott <matt@west.co.tt>
2024-07-04 13:30:51 +0100

    Add admin view for deleting forum posts

--- forums/tests/test_forum_views.py
+++ forums/tests/test_forum_views.py
@@ -78,3 +78,37 @@ class TestTopicReply(TestCase):
         self.assertEqual(Topic.objects.get(id=1).reply_count, 1)
         post = Post.objects.get(body="it's only a bit broken")
         self.assertRedirects(response, '/forums/1/#post-%d' % post.id)
+
+
+class TestDeletePost(TestCase):
+    fixtures = ['tests/forum.json']
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', email='testuser@example.com', password='12345', is_staff=True
+        )
+        self.client.login(username='testuser', password='12345')
+        self.post = Post.objects.create(topic_id=1, user_id=1, body="it's only a bit broken")
+
+    def test_get(self):
+        response = self.client.get('/forums/post/%d/delete/' % self.post.id)
+        self.assertEqual(response.status_code, 200)
+
+    def test_post(self):
+        response = self.client.post(
+            '/forums/post/%d/delete/' % self.post.id,
+            {'yes': 'yes'},
+        )
+        self.assertRedirects(response, '/forums/1/')
+        self.assertFalse(Post.objects.filter(id=self.post.id).exists())
+
+    def test_delete_requires_admin_privileges(self):
+        self.user.is_staff = False
+        self.user.save()
+
+        response = self.client.post(
+            '/forums/post/%d/delete/' % self.post.id,
+            {'yes': 'yes'},
+        )
+        self.assertTrue(Post.objects.filter(id=self.post.id).exists())
+        self.assertRedirects(response, '/forums/post/%d/' % self.post.id)
