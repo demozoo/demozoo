ac2056d99fe5f452d330600ce82e2f1579f3d5bc
Matt Westcott <matt@west.co.tt>
2024-07-05 17:55:52 +0200

    Repurpose ListDetailModelViewSet for the get_serializer logic

--- api/views/generic.py
+++ api/views/generic.py
@@ -13,14 +13,15 @@ from productions.models import Production, ProductionType
 
 class ListDetailModelViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    extension to ReadOnlyModelViewSet to allow us to have different serializer classes for
-    list and detail views. (seriously, we can't do this out of the box?)
+    Extension to ReadOnlyModelViewSet to allow us to have different field lists for
+    list and detail views. Requires the serializer to inherit OutputFieldsMixin
     """
-    def get_serializer_class(self):
-        if self.action == 'list':
-            return self.list_serializer_class
-        else:
-            return self.serializer_class
+    listing_fields = None
+
+    def get_serializer(self, *args, **kwargs):
+        if self.action == 'list' and self.listing_fields is not None:
+            kwargs['fields'] = self.listing_fields
+        return super().get_serializer(*args, **kwargs)
 
 
 class PlatformViewSet(viewsets.ReadOnlyModelViewSet):
@@ -37,31 +38,23 @@ class ProductionTypeViewSet(viewsets.ReadOnlyModelViewSet):
     ordering_fields = ['id', 'name', 'path']
 
 
-class ProductionViewSet(viewsets.ReadOnlyModelViewSet):
-    def get_serializer(self, *args, **kwargs):
-        if self.action == 'list':
-            kwargs['fields'] = serializers.PRODUCTION_LISTING_FIELDS
-        return super().get_serializer(*args, **kwargs)
-
+class ProductionViewSet(ListDetailModelViewSet):
     queryset = Production.objects.prefetch_related(
         'platforms', 'types', 'author_nicks__releaser', 'author_affiliation_nicks__releaser', 'tags'
     )
     serializer_class = serializers.ProductionSerializer
     filterset_class = filters.ProductionFilter
     ordering_fields = ['id', 'sortable_title', 'release_date_date', 'supertype']
+    listing_fields = serializers.PRODUCTION_LISTING_FIELDS
 
 
-class ReleaserViewSet(viewsets.ReadOnlyModelViewSet):
+class ReleaserViewSet(ListDetailModelViewSet):
     queryset = Releaser.objects.all()
     serializer_class = serializers.ReleaserSerializer
     filterset_class = filters.ReleaserFilter
     lookup_value_regex = r'\d+'
     ordering_fields = ['id', 'name']
-
-    def get_serializer(self, *args, **kwargs):
-        if self.action == 'list':
-            kwargs['fields'] = ['url', 'id', 'name', 'is_group']
-        return super().get_serializer(*args, **kwargs)
+    listing_fields = ['url', 'id', 'name', 'is_group']
 
     @action(detail=True)
     def productions(self, request, pk):
@@ -88,40 +81,28 @@ class ReleaserViewSet(viewsets.ReadOnlyModelViewSet):
         return Response(serializer.data)
 
 
-class PartySeriesViewSet(viewsets.ReadOnlyModelViewSet):
+class PartySeriesViewSet(ListDetailModelViewSet):
     queryset = PartySeries.objects.all()
     serializer_class = serializers.PartySeriesSerializer
     filterset_class = filters.PartySeriesFilter
     ordering_fields = ['id', 'name']
-
-    def get_serializer(self, *args, **kwargs):
-        if self.action == 'list':
-            kwargs['fields'] = serializers.PARTY_SERIES_LISTING_FIELDS
-        return super().get_serializer(*args, **kwargs)
+    listing_fields = serializers.PARTY_SERIES_LISTING_FIELDS
 
 
-class PartyViewSet(viewsets.ReadOnlyModelViewSet):
+class PartyViewSet(ListDetailModelViewSet):
     queryset = Party.objects.all()
     serializer_class = serializers.PartySerializer
     filterset_class = filters.PartyFilter
     ordering_fields = ['id', 'name', 'start_date_date', 'end_date_date', 'country_code']
-
-    def get_serializer(self, *args, **kwargs):
-        if self.action == 'list':
-            kwargs['fields'] = serializers.PARTY_LISTING_FIELDS
-        return super().get_serializer(*args, **kwargs)
+    listing_fields = serializers.PARTY_LISTING_FIELDS
 
 
-class BBSViewSet(viewsets.ReadOnlyModelViewSet):
+class BBSViewSet(ListDetailModelViewSet):
     queryset = BBS.objects.prefetch_related('tags')
     serializer_class = serializers.BBSSerializer
     filterset_class = filters.BBSFilter
     ordering_fields = ['id', 'name']
-
-    def get_serializer(self, *args, **kwargs):
-        if self.action == 'list':
-            kwargs['fields'] = [
-                'url', 'demozoo_url', 'id', 'name', 'location',
-                'latitude', 'longitude', 'tags',
-            ]
-        return super().get_serializer(*args, **kwargs)
+    listing_fields = [
+        'url', 'demozoo_url', 'id', 'name', 'location',
+        'latitude', 'longitude', 'tags',
+    ]
