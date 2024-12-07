1e3c4a09a63cea4620cc57f733a092baf74b377a
Matt Westcott <matt@west.co.tt>
2024-12-07 19:42:26 +0000

    Move byline template tag to production_tags

--- productions/templatetags/production_tags.py
+++ productions/templatetags/production_tags.py
@@ -4,6 +4,15 @@ from django import template
 register = template.Library()
 
 
+@register.inclusion_tag("productions/tags/byline.html")
+def byline(production):
+    return {
+        "unparsed_byline": production.unparsed_byline,
+        "authors": [(nick, nick.releaser) for nick in production.author_nicks_with_authors()],
+        "affiliations": [(nick, nick.releaser) for nick in production.author_affiliation_nicks_with_groups()],
+    }
+
+
 @register.inclusion_tag("productions/tags/core_details.html", takes_context=True)
 def production_core_details(context, production, subpage=False):
     ctx = {
