e30ee168d0cf5c12a603016a296c1566a7b37bbe
Matt Westcott <matt@west.co.tt>
2024-12-07 23:54:45 +0000

    Move screenshot thumbnail template tags to ui_tags

--- common/templatetags/ui_tags.py
+++ common/templatetags/ui_tags.py
@@ -13,3 +13,35 @@ def icon(icon):
 def location_flag(obj):
     # obj = any model instance inheriting from LocationMixin
     return {"obj": obj}
+
+
+def thumbnail_params_for_size(screenshot, target_width, target_height):
+    width, height = screenshot.thumb_dimensions_to_fit(target_width, target_height)
+
+    return {
+        "url": screenshot.thumbnail_url,
+        "width": width,
+        "height": height,
+        "natural_width": screenshot.thumbnail_width or 1,
+        "natural_height": screenshot.thumbnail_height or 1,
+    }
+
+
+@register.inclusion_tag("tags/thumbnail.html")
+def thumbnail(screenshot):
+    return thumbnail_params_for_size(screenshot, 133, 100)
+
+
+@register.inclusion_tag("tags/microthumb.html")
+def microthumb(screenshot):
+    return thumbnail_params_for_size(screenshot, 48, 36)
+
+
+@register.inclusion_tag("tags/thumbnail.html")
+def megathumb(screenshot):
+    width, height = screenshot.thumb_dimensions_to_fit(400, 300)
+    return {
+        "url": screenshot.standard_url,
+        "width": width,
+        "height": height,
+    }
--- demoscene/templatetags/demoscene_tags.py
+++ demoscene/templatetags/demoscene_tags.py
@@ -52,38 +52,6 @@ def last_edited_by(context, item):
     }
 
 
-def thumbnail_params_for_size(screenshot, target_width, target_height):
-    width, height = screenshot.thumb_dimensions_to_fit(target_width, target_height)
-
-    return {
-        "url": screenshot.thumbnail_url,
-        "width": width,
-        "height": height,
-        "natural_width": screenshot.thumbnail_width or 1,
-        "natural_height": screenshot.thumbnail_height or 1,
-    }
-
-
-@register.inclusion_tag("tags/thumbnail.html")
-def thumbnail(screenshot):
-    return thumbnail_params_for_size(screenshot, 133, 100)
-
-
-@register.inclusion_tag("tags/microthumb.html")
-def microthumb(screenshot):
-    return thumbnail_params_for_size(screenshot, 48, 36)
-
-
-@register.inclusion_tag("tags/thumbnail.html")
-def megathumb(screenshot):
-    width, height = screenshot.thumb_dimensions_to_fit(400, 300)
-    return {
-        "url": screenshot.standard_url,
-        "width": width,
-        "height": height,
-    }
-
-
 @register.simple_tag
 def site_stats():
     return {
