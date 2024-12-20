9479f2ba7087563df417327c15abd153c51ea033
Matt Westcott <matt@west.co.tt>
2024-12-20 18:12:07 +0000

    Add a data-edit-form attribute that applies form edit controls as standard, and use it on various forms instead of matching on classname

--- productions/templates/productions/edit_credit.js
+++ productions/templates/productions/edit_credit.js
@@ -1,4 +1,4 @@
 function(modal) {
-    modal.ajaxifyForm($('form.edit_credit_form', modal.body));
+    modal.ajaxifyForm($('[data-edit-form]', modal.body));
     modal.ajaxifyLink($('a.delete_credit', modal.body));
 }
