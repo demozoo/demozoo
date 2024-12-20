aa88a0913a78c42457228894c28989f14e8c6006
Matt Westcott <matt@west.co.tt>
2024-12-20 18:19:00 +0000

    Remove unused releasers/edit-credit view

--- demoscene/tests/test_releaser_views.py
+++ demoscene/tests/test_releaser_views.py
@@ -42,50 +42,6 @@ class TestAddCredit(TestCase):
         self.assertEqual(Credit.objects.filter(production=self.pondlife, nick=self.laesq.primary_nick).count(), 1)
 
 
-class TestEditCredit(TestCase):
-    fixtures = ["tests/gasman.json"]
-
-    def setUp(self):
-        self.user = User.objects.create_user(username="testuser", password="12345")
-        self.client.login(username="testuser", password="12345")
-        self.gasman = Releaser.objects.get(name="Gasman")
-        self.papaya_dezign = Releaser.objects.get(name="Papaya Dezign")
-        self.pondlife = Production.objects.get(title="Pondlife")
-
-    def test_locked(self):
-        response = self.client.get(
-            "/releasers/%d/edit_credit/%d/%d/"
-            % (self.papaya_dezign.id, self.papaya_dezign.primary_nick.id, self.pondlife.id)
-        )
-        self.assertEqual(response.status_code, 403)
-
-    def test_get(self):
-        response = self.client.get(
-            "/releasers/%d/edit_credit/%d/%d/" % (self.gasman.id, self.gasman.primary_nick.id, self.pondlife.id)
-        )
-        self.assertEqual(response.status_code, 200)
-
-    def test_post(self):
-        credit = Credit.objects.get(nick=self.gasman.primary_nick, production=self.pondlife)
-        response = self.client.post(
-            "/releasers/%d/edit_credit/%d/%d/" % (self.gasman.id, self.gasman.primary_nick.id, self.pondlife.id),
-            {
-                "nick": self.gasman.primary_nick.id,
-                "production_name": "Pondlife",
-                "production_id": self.pondlife.id,
-                "credit-TOTAL_FORMS": 1,
-                "credit-INITIAL_FORMS": 1,
-                "credit-MIN_NUM_FORMS": 0,
-                "credit-MAX_NUM_FORMS": 1000,
-                "credit-0-id": credit.id,
-                "credit-0-category": "Music",
-                "credit-0-role": "",
-            },
-        )
-        self.assertRedirects(response, "/sceners/%d/" % self.gasman.id)
-        self.assertEqual(Credit.objects.get(production=self.pondlife, nick=self.gasman.primary_nick).category, "Music")
-
-
 class TestDeleteCredit(TestCase):
     fixtures = ["tests/gasman.json"]
 
