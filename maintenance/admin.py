007032b2afee0cbecc0c20e16a015b1f7a3b4a20
Andry Joos <4256370+ajoos@users.noreply.github.com>
2025-06-04 21:29:49 +0200

    Add additional descriptions for /admin

--- maintenance/admin.py
+++ maintenance/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from maintenance.models import UnsafeLink
 
+class UnsafeLinkAdmin(admin.ModelAdmin):
+    list_display=["url_part"]
+    # Custom description in the admin interface
+    fieldsets = (
+        (None, {
+            'fields': ('url_part',)
+        }),
+        ('Instructions', {
+            'description': 'Add parts of URLs that you identify as unsafe. "Unsafe links" report will use these to list'
+            'unsafe production links',
+            'fields': (),
+        })
+    )
 
-admin.site.register(UnsafeLink, list_display=["url_part"])
+admin.site.register(UnsafeLink, UnsafeLinkAdmin)
