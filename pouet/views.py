d2c0a23f028672b3a6ffd4f19023762200b5a62e
Andry Joos <4256370+ajoos@users.noreply.github.com>
2025-06-15 21:05:06 +0200

    Removed unused import

--- pouet/views.py
+++ pouet/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
