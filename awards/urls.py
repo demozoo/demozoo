97f7cbc26e75b9e96aadb26bfceb84372f6655ce
Matt Westcott <matt@west.co.tt>
2025-07-13 00:12:46 +0100

    Implement changing decisions from review page

--- awards/urls.py
+++ awards/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 
-from awards.views import candidates, recommend, remove_recommendation, report, screening, screening_review, show
+from awards.views import (
+    candidates,
+    recommend,
+    remove_recommendation,
+    report,
+    screening,
+    screening_review,
+    screening_review_change,
+    show,
+)
 
 
 urlpatterns = [
@@ -8,6 +17,12 @@ urlpatterns = [
     path("<slug:event_slug>/", show, {}, "award"),
     path("<slug:event_slug>/screening/", screening, {}, "awards_screening"),
     path("<slug:event_slug>/screening/review/", screening_review, {}, "awards_screening_review"),
+    path(
+        "<slug:event_slug>/screening/review/<int:decision_id>/",
+        screening_review_change,
+        {},
+        "awards_screening_review_change",
+    ),
     path("remove_recommendation/<int:recommendation_id>/", remove_recommendation, {}, "awards_remove_recommendation"),
     path("<slug:event_slug>/report/<int:category_id>/", report, {}, "awards_report"),
     path("<slug:event_slug>/<slug:category_slug>/", candidates, {}, "awards_candidates"),
