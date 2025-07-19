35ba694d8421cd4199cf3ce977bb8cfd654e6083
Matthew Westcott <matthew@west.co.tt>
2025-07-19 19:58:18 +0200

    Add screening view for a specific prod

--- awards/urls.py
+++ awards/urls.py
@@ -6,6 +6,7 @@ from awards.views import (
     remove_recommendation,
     report,
     screening,
+    screening_production,
     screening_review,
     screening_review_change,
     show,
@@ -16,6 +17,7 @@ urlpatterns = [
     path("<slug:event_slug>/recommend/<int:production_id>/", recommend, {}, "awards_recommend"),
     path("<slug:event_slug>/", show, {}, "award"),
     path("<slug:event_slug>/screening/", screening, {}, "awards_screening"),
+    path("<slug:event_slug>/screening/<int:production_id>/", screening_production, {}, "awards_screening_production"),
     path("<slug:event_slug>/screening/review/", screening_review, {}, "awards_screening_review"),
     path(
         "<slug:event_slug>/screening/review/<int:decision_id>/",
