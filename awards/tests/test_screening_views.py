d227ed4578ad748357ce81af0cdad03febf308d3
Matthew Westcott <matthew@west.co.tt>
2026-01-09 16:20:57 +0100

    Add 'at least one yay' to screening filter options
    This allows us to repurpose the filtering mechanism to produce the final screened prods report

--- awards/tests/test_screening_views.py
+++ awards/tests/test_screening_views.py
@@ -195,6 +195,32 @@ class TestScreening(TestCase):
         self.assertRedirects(response, "/awards/meteoriks-2020/?rating_count=N")
         self.assertContains(response, "There are no productions that fit the chosen criteria.")
 
+    def test_filter_by_rating_count_at_least_one_yay(self):
+        other_juror = User.objects.create_user(username="other_juror", password="54321")
+        self.meteoriks.jurors.create(user=other_juror)
+        other_other_juror = User.objects.create_user(username="other_other_juror", password="54321")
+        self.meteoriks.jurors.create(user=other_other_juror)
+
+        self.client.login(username="juror", password="67890")
+        url = "/awards/meteoriks-2020/screening/?rating_count=Y"
+
+        # prods with no decisions should not show
+        response = self.client.get(url, follow=True)
+        self.assertRedirects(response, "/awards/meteoriks-2020/?rating_count=Y")
+        self.assertContains(response, "There are no productions that fit the chosen criteria.")
+
+        # prods with a Yay should show
+        prod = Production.objects.get(title="The Brexecutable Music Compo Is Over")
+        self.meteoriks.screening_decisions.create(
+            user=other_juror,
+            production=prod,
+            is_accepted=True,
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        # form action url should preserve the filter
+        self.assertContains(response, f'action="{url}"')
+
     def test_filter_by_platform_group(self):
         oldschool = self.meteoriks.series.platform_groups.get(name="Oldschool")
         newschool = self.meteoriks.series.platform_groups.get(name="Newschool")
