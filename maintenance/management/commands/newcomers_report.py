9441c27f5950f04fa61f750a13d86d19809df888
Matthew Westcott <matthew@west.co.tt>
2026-01-21 22:48:42 +0000

    Update newcomers report to include screening counts

--- maintenance/management/commands/newcomers_report.py
+++ maintenance/management/commands/newcomers_report.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand
 from django.db import connection
-from django.db.models import Min
+from django.db.models import Q
 
+from awards.models import ScreeningDecision
 from demoscene.models import Releaser
 from productions.models import ProductionType
 
@@ -26,7 +27,7 @@ class Command(BaseCommand):
             cursor.execute(
                 """
 SELECT DISTINCT releaser_id FROM (
-    SELECT DISTINCT demoscene_nick.releaser_id
+    SELECT demoscene_nick.releaser_id
     FROM productions_production
     INNER JOIN productions_production_author_nicks ON (
         productions_production.id = productions_production_author_nicks.production_id
@@ -46,29 +47,9 @@ SELECT DISTINCT releaser_id FROM (
         AND productions_production.id NOT IN (
             SELECT production_id FROM parties_competitionplacing WHERE competition_id = ANY(%(exclude_compo_ids)s)
         )
+    GROUP BY demoscene_nick.releaser_id
     UNION
-    SELECT DISTINCT demoscene_nick.releaser_id
-    FROM productions_production
-    INNER JOIN productions_production_author_affiliation_nicks ON (
-        productions_production.id = productions_production_author_affiliation_nicks.production_id
-    )
-    INNER JOIN demoscene_nick ON (
-        productions_production_author_affiliation_nicks.nick_id = demoscene_nick.id
-    )
-    LEFT JOIN productions_production_types ON (
-        productions_production.id = productions_production_types.production_id
-    )
-    WHERE
-        EXTRACT(YEAR FROM release_date_date) = %(year)s
-        AND (
-            productions_production.supertype = 'production'
-            OR productions_production_types.productiontype_id = ANY(%(exe_gfx_ids)s)
-        )
-        AND productions_production.id NOT IN (
-            SELECT production_id FROM parties_competitionplacing WHERE competition_id = ANY(%(exclude_compo_ids)s)
-        )
-    UNION
-    SELECT DISTINCT demoscene_nick.releaser_id
+    SELECT demoscene_nick.releaser_id
     FROM productions_production
     INNER JOIN productions_credit ON (
         productions_production.id = productions_credit.production_id
@@ -88,6 +69,7 @@ SELECT DISTINCT releaser_id FROM (
         AND productions_production.id NOT IN (
             SELECT production_id FROM parties_competitionplacing WHERE competition_id = ANY(%(exclude_compo_ids)s)
         )
+    GROUP BY demoscene_nick.releaser_id
 ) AS releasers_this_year;
             """,
                 {"year": year, "exe_gfx_ids": exe_gfx_ids, "exclude_compo_ids": exclude_compo_ids},
@@ -96,17 +78,51 @@ SELECT DISTINCT releaser_id FROM (
 
         for releaser_id in releasers_this_year:
             releaser = Releaser.objects.get(id=releaser_id)
+            prod_ids = set()
+            is_eligible = True
 
-            author_rel_date = releaser.productions().aggregate(rel_date=Min("release_date_date"))["rel_date"]
-            if author_rel_date and author_rel_date.year < year:
+            author_prods = (
+                releaser.productions()
+                .filter(Q(supertype="production") | Q(types__in=exe_gfx_ids))
+                .values_list("id", "release_date_date")
+            )
+            for prod_id, rel_date in author_prods:
+                if not rel_date or rel_date.year < year:
+                    is_eligible = False
+                    break
+                prod_ids.add(prod_id)
+            if not is_eligible:
                 continue
 
-            affil_rel_date = releaser.member_productions().aggregate(rel_date=Min("release_date_date"))["rel_date"]
-            if affil_rel_date and affil_rel_date.year < year:
+            affil_prods = (
+                releaser.member_productions()
+                .filter(Q(supertype="production") | Q(types__in=exe_gfx_ids))
+                .values_list("id", "release_date_date")
+            )
+            for prod_id, rel_date in affil_prods:
+                if not rel_date or rel_date.year < year:
+                    is_eligible = False
+                    break
+                prod_ids.add(prod_id)
+            if not is_eligible:
                 continue
 
-            credit_rel_date = releaser.credits().aggregate(rel_date=Min("production__release_date_date"))["rel_date"]
-            if credit_rel_date and credit_rel_date.year < year:
+            credit_prods = (
+                releaser.credits()
+                .filter(Q(production__supertype="production") | Q(production__types__in=exe_gfx_ids))
+                .values_list("production_id", "production__release_date_date")
+            )
+            for prod_id, rel_date in credit_prods:
+                if not rel_date or rel_date.year < year:
+                    is_eligible = False
+                    break
+                prod_ids.add(prod_id)
+            if not is_eligible:
                 continue
 
-            print("%s,https://demozoo.org%s" % (releaser.name, releaser.get_absolute_url()))
+            yay_count = ScreeningDecision.objects.filter(
+                production_id__in=prod_ids,
+                is_accepted=True,
+            ).count()
+
+            print("%s,https://demozoo.org%s,%d" % (releaser.name, releaser.get_absolute_url(), yay_count))
--- maintenance/tests/test_commands.py
+++ maintenance/tests/test_commands.py
@@ -2,14 +2,17 @@ from django.core.management import call_command
 from django.test import TestCase
 from django.test.utils import captured_stdout
 
-from demoscene.models import Nick
-from productions.models import Production
+from demoscene.models import Nick, Releaser
+from productions.models import Production, ProductionType
 
 
 class TestNewcomersReport(TestCase):
     fixtures = ["tests/gasman.json"]
 
     def test_run(self):
+        rez = Releaser.objects.create(name="Rez", is_group=False)
+        razor_1911 = Releaser.objects.create(name="Razor 1911", is_group=True)
+
         madrielle = Production.objects.get(title="Madrielle")
         madrielle.author_affiliation_nicks.add(Nick.objects.get(name="Future Crew"))
         madrielle.credits.create(nick=Nick.objects.get(name="Ra"), role="code")
@@ -18,6 +21,27 @@ class TestNewcomersReport(TestCase):
         brexecutable.credits.create(nick=Nick.objects.get(name="Abyss"), role="code")
         brexecutable.credits.create(nick=Nick.objects.get(name="Future Crew"), role="code")
         brexecutable.credits.create(nick=Nick.objects.get(name="Ra"), role="code")
+        brexecutable.author_nicks.add(Nick.objects.get(name="Rez"))
+        brexecutable.author_nicks.add(Nick.objects.get(name="Razor 1911"))
+
+        votedisk = Production.objects.create(
+            title="We have accidentally borrowed your votedisk",
+            release_date_date="2019-05-01",
+            release_date_precision="d",
+            supertype="production",
+        )
+        votedisk.types.add(ProductionType.objects.get(name="64K Intro"))
+        votedisk.author_nicks.add(Nick.objects.get(name="Rez"))
+        votedisk.author_affiliation_nicks.add(Nick.objects.get(name="Razor 1911"))
+
         with captured_stdout() as stdout:
             call_command("newcomers_report", "2019")
-        self.assertEqual(stdout.getvalue(), "Abyss,https://demozoo.org/sceners/8/\n")
+        lines = stdout.getvalue().strip().split("\n")
+        self.assertCountEqual(
+            lines,
+            [
+                "Abyss,https://demozoo.org/sceners/8/,0",
+                "Razor 1911,https://demozoo.org/groups/%d/,0" % razor_1911.id,
+                "Rez,https://demozoo.org/sceners/%d/,0" % rez.id,
+            ],
+        )
